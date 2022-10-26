ab6467ee7f7f73fffc65442592046c4f94300538
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-26 21:08:36 +0300

    Frontend: Adding text input for email and password

--- onYourWay/app/screens/LoginScreen/Login.jsx
+++ onYourWay/app/screens/LoginScreen/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, Image, SafeAreaView } from "react-native";
+import { Text, Image, SafeAreaView, TextInput } from "react-native";
+import AppButton from "../../components/AppButton/AppButton";
 import styles from "./styles";
 
 function Login() {
@@ -10,6 +11,21 @@ function Login() {
         style={styles.logo}
         source={require("../../assets/logo-white.png")}
       />
+
+      <TextInput
+        clearButtonMode="always"
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        clearButtonMode="always"
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <AppButton value={"Login"} onPress={() => console.log("login")} />
     </SafeAreaView>
   );
 }
