6a9cdae5c207ee24ac3689d75866023eb8269d09
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-26 21:03:51 +0300

    Frontend:Adding styles for the custom app button

--- onYourWay/app/components/AppButton/styles.js
+++ onYourWay/app/components/AppButton/styles.js
@@ -1,1 +1,20 @@
- 
+import { StyleSheet } from "react-native";
+import colors from "../../config/colors";
+import text from "../../config/text";
+
+export default StyleSheet.create({
+  button: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "80%",
+  },
+  text: {
+    color: colors.black,
+    fontSize: text.size,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
