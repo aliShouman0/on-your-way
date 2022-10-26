41a951943183fb74ff8e1188f8b33edec6ddf3d6
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-26 21:10:09 +0300

    Frontend:Styling text input

--- onYourWay/app/screens/LoginScreen/styles.js
+++ onYourWay/app/screens/LoginScreen/styles.js
@@ -5,13 +5,21 @@ export default StyleSheet.create({
   mainView: {
     backgroundColor: colors.primary,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    flex:1,
+    flex: 1,
     alignItems: "center",
-
   },
   logo: {
-    marginTop:20,
+    marginTop: 20,
     width: 300,
-    height: 300, 
+    height: 300,
+  },
+  input: {
+    backgroundColor: colors.white,
+    borderRadius: 5,
+    width: "90%",
+    height: 55,
+    padding: "4%",
+    margin:8,
+    fontSize: 15,
   },
 });
