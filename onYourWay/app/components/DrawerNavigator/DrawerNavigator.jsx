3c43d4ac1f5184d207f756bcf8bd564cb92e969e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-29 21:15:14 +0300

    Frontend: Adding styles for Drawer Navigator

--- onYourWay/app/components/DrawerNavigator/DrawerNavigator.jsx
+++ onYourWay/app/components/DrawerNavigator/DrawerNavigator.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
- 
+import { FontAwesome } from "@expo/vector-icons";
+
+import MyOrder from "../../screens/MyOrderScreen/MyOrder";
+import colors from "../../config/colors";
+import text from "../../config/text";
+import styles from "./styles";
+
 function DrawerNavigator() {
   const Drawer = createDrawerNavigator();
 
@@ -8,10 +14,16 @@ function DrawerNavigator() {
     <Drawer.Navigator
       initialRouteName="MyOrder"
       screenOptions={{
-        headerShown: false, 
+        headerShown: false,
+        drawerActiveBackgroundColor: colors.secondary,
+        drawerActiveTintColor: colors.black,
+        drawerInactiveTintColor: "white",
+        drawerInactiveBackgroundColor: "transparent",
+        drawerLabelStyle: styles.drawerLabelStyle,
+        drawerStyle: styles.drawerStyle,
       }}
     >
-     
+    
     </Drawer.Navigator>
   );
 }
