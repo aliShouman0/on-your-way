30ddd5d9945727eda308e9345a601f5b8245045a
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-29 08:32:45 +0300

    Frontend: Adding Icon to the custom navbar depending on its type from props

--- onYourWay/app/components/Navbar/Navbar.jsx
+++ onYourWay/app/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native"; 
+import { Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 import styles from "./styles";
 
 function Navbar({ type, title, navigation }) {
   return (
     <View style={styles.view}>
-     
+      <TouchableOpacity onPress={() => navigation.pop()}>
+        {type === "register" && (
+          <Ionicons name="arrow-back" size={35} style={styles.icon} />
+        )}
+        {type === "main" && (
+          <FontAwesome5 name="bars" size={35} style={styles.icon} />
+        )}
+      </TouchableOpacity>
 
       <Text style={styles.title}>{title}</Text>
     </View>
