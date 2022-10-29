12109939038a962996d26f829e9d46ed74aeb4a8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-29 21:25:23 +0300

    Frontend: Adding DrawerNavigator to Stack.Navigator to allow navigating when login

--- onYourWay/App.js
+++ onYourWay/App.js
@@ -1,15 +1,21 @@
-import { NavigationContainer, StackActions } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Login from "./app/screens/LoginScreen/Login";
 import NextSignupScreen from "./app/screens/NextSignupScreen/NextSignupScreen";
 import Signup from "./app/screens/SignupScreen/Signup";
+import DrawerNavigator from "./app/components/DrawerNavigator/DrawerNavigator";
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  const Stack = createStackNavigator();
+  const Drawer = createDrawerNavigator();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{}} initialRouteName={"Login"}>
         <Stack.Screen
           name="Login"
           component={Login}
@@ -23,6 +29,11 @@ export default function App() {
         <Stack.Screen
           name="NextSignupScreen"
           component={NextSignupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="DrawerNavigator"
+          component={DrawerNavigator}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
