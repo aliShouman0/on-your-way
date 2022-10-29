dc26feb5ed2638645eb8d638fcea833630953446
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-29 08:38:41 +0300

    Frontend: Styling the custom navbar

---
+++ onYourWay/app/components/Navbar/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import colors from "../../config/colors";
+import text from "../../config/text";
+
+export default StyleSheet.create({
+  view: {
+    flexDirection: "row",
+    height: 65,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: colors.dark,
+  },
+  icon: {
+    color: colors.white,
+    marginLeft: 15,
+    textAlign: "left",
+  },
+  title: {
+    flex:1,
+    color: colors.white,
+    fontSize: text.sizeMid,
+    textAlign: "center",
+    marginRight:50
+  },
+});
