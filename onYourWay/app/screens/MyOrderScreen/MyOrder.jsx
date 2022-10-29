a037bade261ed7aa14450700a6520ffe643d5158
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-29 20:53:29 +0300

    Frontend: Creating my order screen

---
+++ onYourWay/app/screens/MyOrderScreen/MyOrder.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
+
+function MyOrder({ navigation }) {
+  return (
+    <SafeAreaView style={styles.mainView}>
+    </SafeAreaView>
+  );
+}
+
+export default MyOrder;
