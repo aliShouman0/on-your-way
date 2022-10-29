b59320112deaa0bd41cfe666abc4554bbd6c81d1
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-29 20:56:24 +0300

    Frontend: Styling  MyOrder screen

---
+++ onYourWay/app/screens/MyOrderScreen/styles.js
@@ -0,0 +1,11 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+import colors from "../../config/colors"; 
+
+export default StyleSheet.create({
+  mainView: {
+    backgroundColor: colors.primary,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    alignItems: "center",
+  },
+});
