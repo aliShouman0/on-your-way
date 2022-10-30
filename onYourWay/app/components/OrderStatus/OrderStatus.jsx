14d10204eeaa65133c0ace67691760fdf56c1359
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 22:38:10 +0200

    Frontend: Adding bottom sheet and the main view

--- onYourWay/app/components/OrderStatus/OrderStatus.jsx
+++ onYourWay/app/components/OrderStatus/OrderStatus.jsx
@@ -5,7 +5,23 @@ import RBSheet from "react-native-raw-bottom-sheet";
 import styles from "./styles";
 
 function OrderStatus({ refRBSheet }) {
-  return <Text>Hi</Text>;
+  const windowHeight = Dimensions.get("window").height;
+
+  return (
+    <RBSheet
+      ref={refRBSheet}
+      closeOnDragDown={true}
+      closeOnPressMask={true}
+      animationType={"fade"}
+      height={(windowHeight * 3) / 4}
+      customStyles={{
+        draggableIcon: styles.draggableIcon,
+        container: styles.container,
+      }}
+    >
+      <View style={styles.view}></View>
+    </RBSheet>
+  );
 }
 
 export default OrderStatus;
