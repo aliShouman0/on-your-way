296ab5a8890e0d3032333a2f3008fe123cd76b02
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 17:01:19 +0200

    Frontend: Creating getStar function that will return filled and empty star base of the rate number from props

--- onYourWay/app/components/Rate/Rate.jsx
+++ onYourWay/app/components/Rate/Rate.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import styles from "./styles";
 
-function Rate({}) {
-  return <View> </View>;
+function Rate({ rate }) {
+  const getStar = () => {
+    const rows = [];
+    for (let i = 0; i < rate; i++) {
+      rows.push(<Entypo name="star" size={25} style={styles.fillIcon} />);
+    }
+    for (let i = 0; i < 5 - rate; i++) {
+      rows.push(<Entypo name="star" size={25} style={styles.icon} />);
+    }
+    return rows;
+  };
+  return (
+    <View style={styles.view}>
+      <Text style={styles.text}>Rated:</Text>
+      {getStar()}
+    </View>
+  );
 }
 
 export default Rate;
