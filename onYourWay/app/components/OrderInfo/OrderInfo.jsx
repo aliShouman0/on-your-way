358954d757e003d0c23866c5c71ac5ceeb43b912
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 10:16:45 +0200

    Frontend: Creating the first section in order info component that will have the name and picture of the user

--- onYourWay/app/components/OrderInfo/OrderInfo.jsx
+++ onYourWay/app/components/OrderInfo/OrderInfo.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+
+import colors from "../../config/colors";
+import SmallButton from "../SmallButton/SmallButton";
+import styles from "./styles";
 
 function OrderInfo() {
-  return <View></View>;
+  return (
+    <View style={styles.mainView}>
+      <View style={styles.userInfo}>
+        <Image
+          resizeMode="contain"
+          source={require("../../assets/user2.jpg")}
+          style={styles.userImg}
+        />
+        <Text style={styles.userName}>User Name</Text>
+      </View>
+     
+    </View>
+  );
 }
 
 export default OrderInfo;
