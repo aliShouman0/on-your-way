5431f4b526318e3afd1a35758cc89a5ed64692ad
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 21:50:11 +0200

    Frontend: Creating Order Status components and its styles file that will be as a bottom sheet to check or edit order status

---
+++ onYourWay/app/components/OrderStatus/OrderStatus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Dimensions, Image, Text, View } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet";
+
+import styles from "./styles";
+
+function OrderStatus({ refRBSheet }) {
+  return <Text>Hi</Text>;
+}
+
+export default OrderStatus;
---
+++ onYourWay/app/components/OrderStatus/styles.js
@@ -0,0 +1,4 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+import colors from "../../config/colors";
+
+export default StyleSheet.create({});
