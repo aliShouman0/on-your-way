df6d6e7f56b8e6d59e60bf0dba4cc9e05aa55528
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 16:31:23 +0200

    Frontend: Styling user image in userInfo components

--- onYourWay/app/components/userInfo/styles.js
+++ onYourWay/app/components/userInfo/styles.js
@@ -4,9 +4,25 @@ import text from "../../config/text";
 
 const windowHeight = Dimensions.get("window").height;
 export default StyleSheet.create({
-  sheetView: { backgroundColor: colors.primary },
   draggableIcon: {
     backgroundColor: colors.secondary,
   },
-  container: { backgroundColor: colors.primary },
+  container: {
+    backgroundColor: colors.primary,
+  },
+  sheetView: {
+    backgroundColor: colors.primary,
+    flex: 1,
+    flexDirection: "row",
+  },
+  userInfo: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  userImg: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
 });
