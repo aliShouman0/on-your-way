973f2b324ee4a234f040ee91f847eb43a854ef0b
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 17:02:45 +0200

    Frontend: Styling Rate component

--- onYourWay/app/components/Rate/styles.js
+++ onYourWay/app/components/Rate/styles.js
@@ -2,4 +2,19 @@ import { StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  view: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: text.sizeMid,
+    color: colors.white,
+    marginRight: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: { color: colors.white },
+  fillIcon: { color: colors.secondary },
+});
