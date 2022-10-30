bfaee8c539d8c7bbcd3c90b47e0f32be9d85dd54
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 19:43:37 +0200

    Frontend: Styling container and text in the light input component

--- onYourWay/app/components/LightInput/styles.js
+++ onYourWay/app/components/LightInput/styles.js
@@ -2,4 +2,17 @@ import { Platform, StyleSheet, StatusBar } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  inputContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  text: {
+    color: colors.white,
+    fontSize: text.sizeSmall,
+    width: "93%",
+    textAlign: "left",
+    marginBottom: 4,
+    fontWeight: "400",
+  },
+});
