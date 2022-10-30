57c79eeca0f4e228f6c4764922578922ecfbb205
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 19:39:29 +0200

    Frontend: Adding structure/view of light input component and add props

--- onYourWay/app/components/LightInput/LightInput.jsx
+++ onYourWay/app/components/LightInput/LightInput.jsx
@@ -2,8 +2,34 @@ import React from "react";
 import { Text, TextInput, View } from "react-native";
 import styles from "./styles";
 
-function LightInput({}) {
-  return <View style={styles.inputContainer}></View>;
+function LightInput({
+  keyboardType,
+  text,
+  secureTextEntry,
+  value,
+  setValue,
+  placeholder,
+  contextMenuHidden,
+  editable,
+}) {
+  return (
+    <View style={styles.inputContainer}>
+      <Text style={styles.text}>{text}</Text>
+      <TextInput
+        secureTextEntry={secureTextEntry}
+        clearButtonMode="always"
+        keyboardType={keyboardType}
+        style={styles.input}
+        placeholder={placeholder ? placeholder : text}
+        value={value}
+        onChangeText={(text) => setValue(text)}
+        editable={editable}
+        contextMenuHidden={contextMenuHidden}
+      />
+    </View>
+  );
 }
+
+ 
 
 export default LightInput;
