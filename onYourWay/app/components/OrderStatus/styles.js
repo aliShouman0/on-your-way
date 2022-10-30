b621869e2a9c68e0ea48d68e62737cc3617191bf
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 22:38:58 +0200

    Frontend: styling the bottom sheet and the main view of this sheet

--- onYourWay/app/components/OrderStatus/styles.js
+++ onYourWay/app/components/OrderStatus/styles.js
@@ -1,4 +1,17 @@
 import { Platform, StyleSheet, StatusBar } from "react-native";
 import colors from "../../config/colors";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  draggableIcon: {
+    backgroundColor: colors.secondary,
+  },
+  container: {
+    backgroundColor: colors.primary,
+    padding: 15,
+  },
+  view: {
+    backgroundColor: colors.darker,
+    flex: 1,
+    borderRadius: 10,
+  },
+});
