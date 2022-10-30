7d6a4d2d3a51132587695ce71dfc7142bcfeccdb
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 12:55:39 +0200

    Frontend: Adding FlatList in My Order screen to load OrderInfo component on the screen

--- onYourWay/app/screens/MyOrderScreen/MyOrder.jsx
+++ onYourWay/app/screens/MyOrderScreen/MyOrder.jsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 import Navbar from "../../components/Navbar/Navbar";
+import OrderInfo from "../../components/OrderInfo/OrderInfo";
 import styles from "./styles";
 
 function MyOrder({ navigation }) {
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"My Order"} navigation={navigation} />
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(data) => data.id.toString()}
+        data={testing}
+        renderItem={({ item, index, separators }) => (
+          <OrderInfo
+            key={item.id}
+            id={item.id}
+            userName={item.userName}
+            userImg={item.userImg}
+            from={item.from}
+            to={item.to}
+            pay={item.pay}
+            orderImg={item.orderImg}
+            orderDescription={item.orderDescription}
+          />
+        )}
+      />
     </SafeAreaView>
   );
 }
