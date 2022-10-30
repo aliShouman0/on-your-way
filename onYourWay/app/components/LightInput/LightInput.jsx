abcb687e3ddf3df94fcd0a39a9dc8c39ba40827a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 19:37:08 +0200

    Frontend: Create lightInput component that will be small and transparent

---
+++ onYourWay/app/components/LightInput/LightInput.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, TextInput, View } from "react-native";
+import styles from "./styles";
+
+function LightInput({}) {
+  return <View style={styles.inputContainer}></View>;
+}
+
+export default LightInput;
---
+++ onYourWay/app/components/LightInput/styles.js
@@ -0,0 +1,5 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+import colors from "../../config/colors";
+import text from "../../config/text";
+
+export default StyleSheet.create({});
