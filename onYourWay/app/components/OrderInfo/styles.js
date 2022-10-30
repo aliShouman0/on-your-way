064a427d1c2dca8bd5e1e736cf26ee30f2b95661
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-30 10:28:43 +0200

    Frontend: Styling the main view/container for order info component

--- onYourWay/app/components/OrderInfo/styles.js
+++ onYourWay/app/components/OrderInfo/styles.js
@@ -2,4 +2,16 @@ import { StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  mainView: {
+    width: "90%",
+    height: 500,
+    backgroundColor: colors.darker,
+    borderRadius: 15,
+    marginVertical: 20,
+    padding: 15,
+    flex: 1,
+    alignSelf: "center",
+  },
+ 
+});
