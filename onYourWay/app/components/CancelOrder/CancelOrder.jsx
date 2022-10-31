73b65dad277a57c314ac2a55f09a9dc8fe212c77
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-31 23:55:53 +0200

    Frontend:Adding Custom input component in Cancel Order component  and create it's states

--- onYourWay/app/components/CancelOrder/CancelOrder.jsx
+++ onYourWay/app/components/CancelOrder/CancelOrder.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useState } from "react";
+import { Dimensions, Text, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
 import Input from "../Input/Input";
 
 import styles from "./styles";
 function CancelOrder({ refRBSheet }) {
+  const [reason, setReason] = useState("");
   const windowHeight = Dimensions.get("window").height;
 
   return (
@@ -19,11 +20,18 @@ function CancelOrder({ refRBSheet }) {
         container: styles.container,
       }}
     >
-      <View styles={styles.view}>
-        <Text styles={styles.textTitle}>Are you sure ?</Text>
-        <Text styles={styles.textSmall}>
+      <View style={styles.view}>
+        <Text style={styles.textTitle}>Are you sure ?</Text>
+        <Text style={styles.textSmall}>
           Please specify why You are cancelling
         </Text>
+        <Input
+          text="Reason"
+          value={reason}
+          setValue={setReason}
+          multiline={true}
+          style={styles.input}
+        />
       </View>
     </RBSheet>
   );
