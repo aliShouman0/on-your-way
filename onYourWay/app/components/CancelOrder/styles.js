3fc6ece76fcc0af6cc292eb3496bcc333a936424
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-31 23:58:00 +0200

    Frontend:Styling text and input in Cancel Order component

--- onYourWay/app/components/CancelOrder/styles.js
+++ onYourWay/app/components/CancelOrder/styles.js
@@ -1,6 +1,8 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import colors from "../../config/colors";
+import text from "../../config/text";
 
+const windowHeight = Dimensions.get("window").height;
 export default StyleSheet.create({
   draggableIcon: {
     backgroundColor: colors.secondary,
@@ -8,5 +10,27 @@ export default StyleSheet.create({
   container: {
     backgroundColor: colors.primary,
     padding: 15,
+  },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+  textSmall: {
+    fontSize: text.sizeSmall,
+    fontWeight: "200",
+    color: colors.white,
+    marginVertical: 10,
+  },
+  textTitle: {
+    fontSize: text.sizeBig,
+    fontWeightL: "500",
+    color: colors.white,
+  },
+  input: {
+    height: "auto",
+    textAlignVertical: "top",
+    maxHeight: (windowHeight * 1) / 5,
+    minHeight: 80,
   },
 });
