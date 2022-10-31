346df07df95c9ca727e48261f65c50e03e732b94
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-31 20:57:15 +0200

    Frontend:Creating first section for Order Status component

--- onYourWay/app/components/OrderStatus/OrderStatus.jsx
+++ onYourWay/app/components/OrderStatus/OrderStatus.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-import { Dimensions, Image, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Dimensions, Image, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
 
 import styles from "./styles";
 
-function OrderStatus({ refRBSheet }) {
+function OrderStatus({ refRBSheet, isReceiver }) {
   const windowHeight = Dimensions.get("window").height;
 
   return (
-    <RBSheet
-      ref={refRBSheet}
-      closeOnDragDown={true}
-      closeOnPressMask={true}
-      animationType={"fade"}
-      height={(windowHeight * 3) / 4}
-      customStyles={{
-        draggableIcon: styles.draggableIcon,
-        container: styles.container,
-      }}
-    >
-      <View style={styles.view}></View>
-    </RBSheet>
+    <>
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        animationType={"fade"}
+        height={(windowHeight * 3) / 4}
+        customStyles={{
+          draggableIcon: styles.draggableIcon,
+          container: styles.container,
+        }}
+      >
+        <View style={styles.view}>
+          <View style={styles.imageContainer}>
+            <Image
+              resizeMode="stretch"
+              source={require("../../assets/keyboard.jpg")}
+              style={styles.img}
+            />
+            <Image
+              resizeMode="stretch"
+              source={require("../../assets/keyboard.jpg")}
+              style={styles.img}
+            />
+          </View>
+        </View>
+      </RBSheet>
+    </>
   );
 }
 
