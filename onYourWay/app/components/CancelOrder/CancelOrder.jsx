582e995e84da128e7cf07f86fd01430ac6a55021
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-31 21:43:37 +0200

    Frontend:Adding RBSheet 'bottom sheet ' in Cancel Order components

--- onYourWay/app/components/CancelOrder/CancelOrder.jsx
+++ onYourWay/app/components/CancelOrder/CancelOrder.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Dimensions } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet";
+
 import styles from "./styles";
 function CancelOrder({ refRBSheet, isReceiver }) {
-  return <></>;
+  const windowHeight = Dimensions.get("window").height;
+
+  return (
+    <RBSheet
+      ref={refRBSheet}
+      closeOnDragDown={true}
+      closeOnPressMask={true}
+      animationType={"fade"}
+      height={(windowHeight * 1) / 2}
+      customStyles={{
+        draggableIcon: styles.draggableIcon,
+        container: styles.container,
+      }}
+    ></RBSheet>
+  );
 }
 
 export default CancelOrder;
