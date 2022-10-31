5fa6aae6f5890e34acf8b5ab04444d3d1f88500d
Ali Alrida Shouman <alishouman010@gmail.com>
2022-10-31 21:24:12 +0200

    Frontend:Styling images and order stutes section

--- onYourWay/app/components/OrderStatus/styles.js
+++ onYourWay/app/components/OrderStatus/styles.js
@@ -1,5 +1,8 @@
-import { Platform, StyleSheet, StatusBar } from "react-native";
+import { Platform, StyleSheet, StatusBar, Dimensions } from "react-native";
 import colors from "../../config/colors";
+import text from "../../config/text";
+
+const windowWidth = Dimensions.get("window").width;
 
 export default StyleSheet.create({
   draggableIcon: {
@@ -12,6 +15,30 @@ export default StyleSheet.create({
   view: {
     backgroundColor: colors.darker,
     flex: 1,
+    justifyContent: "center",
     borderRadius: 10,
+    alignItems: "center",
   },
+  imageContainer: {
+    flex: 5,
+    width: "100%",
+    height: "100%",
+    marginVertical: 15,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    height: "48%",
+    width: "90%",
+    margin: 5,
+  },
+  orderStatus: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "30%",
+    marginVertical: 15,
+  }
 });
