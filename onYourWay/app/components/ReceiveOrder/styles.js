e749c74d8443a16503b94e598c8dbebe9fd22c48
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 01:10:37 +0200

    Frontend: Styling the view of bottom sheet in Receive Order component

--- onYourWay/app/components/ReceiveOrder/styles.js
+++ onYourWay/app/components/ReceiveOrder/styles.js
@@ -2,4 +2,12 @@ import { StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  draggableIcon: {
+    backgroundColor: colors.secondary,
+  },
+  container: {
+    backgroundColor: colors.primary,
+    padding: 15,
+  },
+});
