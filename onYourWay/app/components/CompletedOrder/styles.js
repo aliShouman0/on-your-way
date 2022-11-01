c89cdec7d0f0d5932b9c6727b0203c247c723254
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 12:52:26 +0200

    Frontend: Styling bottom view container and icon

--- onYourWay/app/components/CompletedOrder/styles.js
+++ onYourWay/app/components/CompletedOrder/styles.js
@@ -2,4 +2,17 @@ import { Dimensions, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+ export default StyleSheet.create({
+  draggableIcon: {
+    backgroundColor: colors.secondary,
+  },
+  container: {
+    backgroundColor: colors.primary,
+    padding: 15,
+  },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  }, 
+});
