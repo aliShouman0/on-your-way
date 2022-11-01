da90482d2bc130d4a8a76ba280a4603b0d4d606a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 19:21:25 +0200

    Frontend: Adding search box for DropDown Picker in AddOrder component

--- onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
+++ onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { SafeAreaView, View } from "react-native"; 
+import { SafeAreaView, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 import DropDownPicker from "react-native-dropdown-picker";
 
-function AddOrder({ navigation }) { 
+function AddOrder({ navigation }) {
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
@@ -19,8 +20,18 @@ function AddOrder({ navigation }) {
           setValue={setFrom}
           setItems={setItems}
           textStyle={styles.textStyle}
-          containerStyle={styles.containerStyle} 
-          itemSeparator={false} 
+          containerStyle={styles.containerStyle}
+          style={styles.style}
+          dropDownContainerStyle={styles.dropDownContainerStyle}
+          searchable={true}
+          searchPlaceholder={"City Name"}
+          itemSeparator={false}
+          ArrowUpIconComponent={() => (
+            <AntDesign name="up" size={24} color="black" />
+          )}
+          ArrowDownIconComponent={() => (
+            <AntDesign name="down" size={24} color="black" />
+          )}
         />
       </View>
     </SafeAreaView>
