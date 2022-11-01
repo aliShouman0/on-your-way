28ffef5ad443bdc29d3c56787701aa6e11260dd9
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 19:25:18 +0200

    Frontend: Styling main view and dropdown in AddOrder screen

--- onYourWay/app/screens/AddOrderScreen/styles.js
+++ onYourWay/app/screens/AddOrderScreen/styles.js
@@ -1,5 +1,24 @@
-import { StyleSheet } from "react-native";
+import { Platform, StatusBar, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  mainView: {
+    backgroundColor: colors.primary,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    alignItems: "center",
+    height: "100%",
+  },
+  city: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+  },
+  textStyle: {
+    color: colors.black,
+    fontWeight: "400",
+  },
+ 
+});
