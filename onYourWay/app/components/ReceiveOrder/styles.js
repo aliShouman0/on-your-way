771f5beb7457e49bc7b38e338a8e6f20a9f52352
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 12:06:12 +0200

    Frontend: Styling the main view container in Receive Order component and its text

--- onYourWay/app/components/ReceiveOrder/styles.js
+++ onYourWay/app/components/ReceiveOrder/styles.js
@@ -10,4 +10,24 @@ export default StyleSheet.create({
     backgroundColor: colors.primary,
     padding: 15,
   },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    height: "30%",
+    marginTop: 10,
+  },
+  code: {
+    color: colors.white,
+    fontSize: text.sizeMid,
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    fontSize: text.sizeSmall,
+    marginVertical: 10,
+  },
 });
