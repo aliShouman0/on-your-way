e894386f0c63523b4d32a648e3b85c2fdb2a03d5
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 19:22:37 +0200

    Frontend: Creating state and testing data for AddOrder screen

--- onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
+++ onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
@@ -7,6 +7,22 @@ import styles from "./styles";
 import DropDownPicker from "react-native-dropdown-picker";
 
 function AddOrder({ navigation }) {
+  const [openFrom, setOpenFrom] = useState(false);
+  const [from, setFrom] = useState("From");
+  const [items, setItems] = useState([
+    { label: "Tripoli", value: "Tripoli" },
+    { label: "Beirut", value: "Beirut" },
+    { label: "Sidon", value: "Sidon" },
+    { label: "Tyre", value: "Tyre" },
+    { label: "Jounie", value: "Jounie" },
+    { label: "Zahle", value: "Zahle" },
+    { label: "Nabatiye", value: "Nabatiye" },
+    { label: "Baalbek", value: "Baalbek" },
+    { label: "Amioun", value: "Amioun" },
+    { label: "Baabda", value: "Baabda" },
+    { label: "Marjayoun", value: "Marjayoun" },
+    { label: "Jdaidet el Matn", value: "Jdaidet" },
+  ]);
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
@@ -24,7 +40,7 @@ function AddOrder({ navigation }) {
           style={styles.style}
           dropDownContainerStyle={styles.dropDownContainerStyle}
           searchable={true}
-          searchPlaceholder={"City Name"}
+          searchPlaceholder={"City Name"} 
           itemSeparator={false}
           ArrowUpIconComponent={() => (
             <AntDesign name="up" size={24} color="black" />
