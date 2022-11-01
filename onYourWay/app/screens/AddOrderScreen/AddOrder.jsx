7af5f110d2b507078c82ae2802f3cc57a81182ef
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 19:20:03 +0200

    Frontend: Adding DropDown Picker in AddOrder component to select city 'from where'

--- onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
+++ onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native"; 
+
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
+import DropDownPicker from "react-native-dropdown-picker";
 
-function AddOrder({ navigation }) {
+function AddOrder({ navigation }) { 
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+      <View style={styles.city}>
+        <DropDownPicker
+          placeholder="From"
+          open={openFrom}
+          value={from}
+          items={items}
+          setOpen={setOpenFrom}
+          setValue={setFrom}
+          setItems={setItems}
+          textStyle={styles.textStyle}
+          containerStyle={styles.containerStyle} 
+          itemSeparator={false} 
+        />
+      </View>
     </SafeAreaView>
   );
 }
