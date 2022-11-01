99e9fe51d900d3c046f31204cfa8154119b01866
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 12:51:16 +0200

    Frontend: Adding the bottom sheet and its styles

--- onYourWay/app/components/CompletedOrder/CompletedOrder.jsx
+++ onYourWay/app/components/CompletedOrder/CompletedOrder.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
+import { Dimensions, Text, View, TouchableOpacity } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet";
+
+import styles from "./styles";
 function CompletedOrder({ refRBSheet }) {
-  return <></>;
+  return (
+    <RBSheet
+      ref={refRBSheet}
+      closeOnDragDown={true}
+      closeOnPressMask={true}
+      animationType={"fade"}
+      height={windowHeight / 2}
+      customStyles={{
+        draggableIcon: styles.draggableIcon,
+        container: styles.container,
+      }}
+    ></RBSheet>
+  );
 }
 
 export default CompletedOrder;
