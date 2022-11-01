6090899ed86b659aeb238c53f2c7bddec85e0f43
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 18:16:39 +0200

    Frontend: Creating main view and adding nav bar for AddOrder screen

--- onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
+++ onYourWay/app/screens/AddOrderScreen/AddOrder.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
 
-function AddOrder() {
-  return <></>;
+function AddOrder({ navigation }) {
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
 export default AddOrder;
