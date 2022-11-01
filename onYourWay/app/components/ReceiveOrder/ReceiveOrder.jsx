e8647d8c526da8b0ff1006b4fe89fb1c169615ce
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 11:48:05 +0200

    Frontend: Adding a QR code image that will be generated based on a unique code for the current order. This will be used to verify the receipt of the order.

--- onYourWay/app/components/ReceiveOrder/ReceiveOrder.jsx
+++ onYourWay/app/components/ReceiveOrder/ReceiveOrder.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
 import { Dimensions, Text, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
+import QRCode from "react-native-qrcode-svg";
 
 import styles from "./styles";
 function ReceiveOrder({ refRBSheet }) {
+  const [startScan, setStartScan] = useState(false);
+  const [codeValue, setCodeValue] = useState(null);
+
   const windowHeight = Dimensions.get("window").height;
 
   return (
-    <RBSheet
-      ref={refRBSheet}
-      closeOnDragDown={true}
-      closeOnPressMask={true}
-      animationType={"fade"}
-      height={(windowHeight * 11) / 20}
-      customStyles={{
-        draggableIcon: styles.draggableIcon,
-        container: styles.container,
-      }}
-    ></RBSheet>
+    <>
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        animationType={"fade"}
+        height={startScan ? (windowHeight * 3) / 4 : (windowHeight * 3) / 5}
+        customStyles={{
+          draggableIcon: styles.draggableIcon,
+          container: styles.container,
+        }}
+      >
+        <View style={styles.view}>
+          <QRCode
+            value={codeValue ? "codeValue" : "error"}
+            logoSize={100}
+            logoBackgroundColor="transparent"
+            backgroundColor="transparent"
+            size={150}
+          />
+        </View>
+      </RBSheet>
+    </>
   );
 }
 
