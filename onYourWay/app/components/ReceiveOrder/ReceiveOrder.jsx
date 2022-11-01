7ab88b45e240461fe79c8c128599e6ab26c22564
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 11:59:57 +0200

    Frontend: Adding Scan QR code functionality that will check permission and scanning code

--- onYourWay/app/components/ReceiveOrder/ReceiveOrder.jsx
+++ onYourWay/app/components/ReceiveOrder/ReceiveOrder.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Dimensions, Text, View, TouchableOpacity } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
 import { FontAwesome } from "@expo/vector-icons";
+import { BarCodeScanner } from "expo-barcode-scanner";
 import QRCode from "react-native-qrcode-svg";
 
 import Input from "../Input/Input";
 import styles from "./styles";
 import AppButton from "../AppButton/AppButton";
 function ReceiveOrder({ refRBSheet }) {
+  const completedOrderRBSheet = useRef();
   const [code, setCode] = useState("");
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
   const [startScan, setStartScan] = useState(false);
   const [codeValue, setCodeValue] = useState(null);
 
   const windowHeight = Dimensions.get("window").height;
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
 
   return (
     <>
@@ -40,11 +52,16 @@ function ReceiveOrder({ refRBSheet }) {
           </Text>
           <Input text="Code" value={code} setValue={setCode} />
           <Text style={styles.text}>Or</Text>
-          <TouchableOpacity style={styles.imgScan} onPress={() => {}}>
+          <TouchableOpacity style={styles.imgScan} onPress={() => scan()}>
             <FontAwesome name="camera" size={45} style={styles.icon} />
             <Text style={styles.scan}>Scan</Text>
           </TouchableOpacity>
-          <AppButton value={"submit"} onPress={() => {}} />
+          <AppButton
+            value={"submit"}
+            onPress={() => {
+              refRBSheet.current.close();
+            }}
+          />
         </View>
       </RBSheet>
     </>
