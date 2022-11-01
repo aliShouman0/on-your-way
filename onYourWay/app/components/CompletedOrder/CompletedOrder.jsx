3396a80f351efc66db84aa346d6c4c9308792a8e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-01 12:54:47 +0200

    Frontend: Adding rate section and its button to modify it

--- onYourWay/app/components/CompletedOrder/CompletedOrder.jsx
+++ onYourWay/app/components/CompletedOrder/CompletedOrder.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { Dimensions, Text, View, TouchableOpacity } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
+import { FontAwesome5 } from "@expo/vector-icons";
 
+import Rate from "../Rate/Rate";
+import Input from "../Input/Input";
+import AppButton from "../AppButton/AppButton";
 import styles from "./styles";
-function CompletedOrder({ refRBSheet }) {
+function CompletedOrder({ refRBSheet }) { 
   return (
     <RBSheet
       ref={refRBSheet}
@@ -15,7 +19,22 @@ function CompletedOrder({ refRBSheet }) {
         draggableIcon: styles.draggableIcon,
         container: styles.container,
       }}
-    ></RBSheet>
+    >
+      <View style={styles.view}>
+        <Text style={styles.textTitle}>Congrats </Text>
+        <View style={styles.rateView}>
+          <Rate rate={rate} styleText={styles.rate} size={28} />
+          <View style={styles.iconView}>
+            <TouchableOpacity onPress={() => setRate(rate + 1)}>
+              <FontAwesome5 name="plus-circle" size={30} style={styles.icon} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setRate(rate - 1)}>
+              <FontAwesome5 name="minus-circle" size={30} style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </RBSheet>
   );
 }
 
