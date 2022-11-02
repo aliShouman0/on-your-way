aeaf0d68a5e50fd64815d2ee34e020d19b3b92ab
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 00:35:05 +0200

    Frontend: Adding FLat list and its data for PickUps screen

--- onYourWay/app/screens/PickUpsScreen/PickUps.js
+++ onYourWay/app/screens/PickUpsScreen/PickUps.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import { FlatList, SafeAreaView, View } from "react-native";
+import OrderInfo from "../../components/OrderInfo/OrderInfo";
 
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
-
+ 
 function PickUps({ navigation }) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
-
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState(testing);
+ 
   return (
     <SafeAreaView style={styles.mainView}>
-      <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+      <Navbar type={"main"} title={"Pick Ups"} navigation={navigation} />
       <View style={styles.container}>
         <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
         <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
       </View>
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(data) => data.id.toString()}
+        data={data}
+        refreshing={refreshing}
+        onRefresh={refresh}
+       
+      />
     </SafeAreaView>
   );
 }
