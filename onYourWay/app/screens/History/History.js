5f62c11d977c9f2ac400f5fea3a63e92de280f67
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 01:23:46 +0200

    Frontend: Adding render Item props for FlatList in the History screen and add refreshing function

--- onYourWay/app/screens/History/History.js
+++ onYourWay/app/screens/History/History.js
@@ -2,14 +2,32 @@ import React, { useState } from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
 import Navbar from "../../components/Navbar/Navbar";
-import OrderInfo from "../../components/OrderInfo/OrderInfo";
+import OrderInfo from "../../components/OrderInfo/OrderInfo"; 
 import styles from "./styles";
+
+ 
 
 function History({ navigation }) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [refreshing, setRefreshing] = useState(false);
   const [data, setData] = useState(testing);
+
+
+  const refresh = () => {
+    setData([
+      {
+        id: "id3",
+        userName: "Refresh Done",
+        userImg: require("../../assets/user1.jpg"),
+        from: "Beirut",
+        to: "Tripoli",
+        pay: "745000L.L",
+        orderImg: require("../../assets/keyboard.jpg"),
+        orderDescription: "Refresh Done",
+      },
+    ]);
+  };
 
   return (
     <SafeAreaView style={styles.mainView}>
@@ -24,6 +42,20 @@ function History({ navigation }) {
         data={data}
         refreshing={refreshing}
         onRefresh={refresh}
+        renderItem={({ item, index, separators }) => (
+          <OrderInfo
+            key={item.id}
+            id={item.id}
+            userName={item.userName}
+            userImg={item.userImg}
+            from={item.from}
+            to={item.to}
+            pay={item.pay}
+            orderImg={item.orderImg}
+            orderDescription={item.orderDescription}
+            completed={true}
+          />
+        )}
       />
     </SafeAreaView>
   );
