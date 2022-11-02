e2e0c8b0726879c79cecf942cc7b3a34a00e2b01
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 09:45:01 +0200

    Frontend: Adding the 2 functions that will get the image from the camera or gallery

--- onYourWay/app/components/UploadImage/UploadImage.jsx
+++ onYourWay/app/components/UploadImage/UploadImage.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import RBSheet from "react-native-raw-bottom-sheet"; 
+import RBSheet from "react-native-raw-bottom-sheet";
+import * as ImagePicker from "expo-image-picker";
+
+import { FontAwesome, Entypo } from "@expo/vector-icons";
 
 import styles from "./styles";
 function UploadImage({ refRBSheet, setImage }) {
-  
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      return result;
+    }
+    return;
+  };
+
+  const camera = async () => {
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+    if (permission.granted == false) {
+      alert(
+        "Camera permission denied Go to settings and allow it to use the camera"
+      );
+      return;
+    }
+    const result = await ImagePicker.launchCameraAsync();
+    if (!result.cancelled) {
+      return result;
+    }
+    return;
+  }; 
   return (
     <RBSheet
       ref={refRBSheet}
