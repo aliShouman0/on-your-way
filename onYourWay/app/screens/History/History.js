3ae5c8da969d2ed23f2c24f3f442b58abc85920a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 01:15:52 +0200

    FrontEnd: Adding DropDown City from searching in history

--- onYourWay/app/screens/History/History.js
+++ onYourWay/app/screens/History/History.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, View } from "react-native";
 
+import DropDownCity from "../../components/DropDownCity/DropDownCity";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
-function History({navigation}) {
+function History({ navigation }) {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"History"} navigation={navigation} />
+      <View style={styles.container}>
+        <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
+        <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
+      </View>
     </SafeAreaView>
   );
 }
