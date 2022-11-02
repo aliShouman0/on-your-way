c6f852def09a29209c03defaf35b3278900cfe39
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 09:41:13 +0200

    Frontend: Adding a bottom sheet in the Upload Image component that will have icons to select camera or gallery

--- onYourWay/app/components/UploadImage/UploadImage.jsx
+++ onYourWay/app/components/UploadImage/UploadImage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet"; 
+
 import styles from "./styles";
 function UploadImage({ refRBSheet, setImage }) {
-  return <></>;
+  
+  return (
+    <RBSheet
+      ref={refRBSheet}
+      closeOnDragDown={true}
+      closeOnPressMask={true}
+      animationType={"fade"}
+      height={150}
+    >
+     
+    </RBSheet>
+  );
 }
 
 export default UploadImage;
