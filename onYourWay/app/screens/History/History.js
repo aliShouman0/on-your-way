7bd82cf2e1498cba0513f3380aa183e9d5371023
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 01:19:21 +0200

    Frontend: Adding FLat list and its states for the History screen

--- onYourWay/app/screens/History/History.js
+++ onYourWay/app/screens/History/History.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
-
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
 import Navbar from "../../components/Navbar/Navbar";
+import OrderInfo from "../../components/OrderInfo/OrderInfo";
 import styles from "./styles";
 
 function History({ navigation }) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState(testing);
 
   return (
     <SafeAreaView style={styles.mainView}>
@@ -16,6 +18,13 @@ function History({ navigation }) {
         <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
         <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
       </View>
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(data) => data.id.toString()}
+        data={data}
+        refreshing={refreshing}
+        onRefresh={refresh}
+      />
     </SafeAreaView>
   );
 }
