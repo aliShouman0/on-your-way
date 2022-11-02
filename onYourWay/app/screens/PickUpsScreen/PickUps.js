dd19bd36c216d46e22066b371cc9a8beffeaf4dc
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 21:23:34 +0200

    Frontend: Adding Drop Down City and its state in the PickUps component

--- onYourWay/app/screens/PickUpsScreen/PickUps.js
+++ onYourWay/app/screens/PickUpsScreen/PickUps.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, View } from "react-native";
+
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
+import DropDownCity from "../../components/DropDownCity/DropDownCity";
 
 function PickUps({ navigation }) {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+      <View style={styles.container}>
+        <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
+        <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
+      </View>
     </SafeAreaView>
   );
 }
