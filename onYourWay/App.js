617bda3bb96de56a8ecfe745f2c37d844f1ce563
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 16:56:09 +0200

    Frontend: Moving Navigation to be in the default directory

--- onYourWay/App.js
+++ onYourWay/App.js
@@ -1,42 +1,10 @@
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import Login from "./app/screens/LoginScreen/Login";
-import NextSignupScreen from "./app/screens/NextSignupScreen/NextSignupScreen";
-import Signup from "./app/screens/SignupScreen/Signup";
-import DrawerNavigator from "./app/components/DrawerNavigator/DrawerNavigator";
-
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "./app/navigation/StackNavigator/StackNavigator";
 
 export default function App() {
-  const Stack = createStackNavigator();
-  const Drawer = createDrawerNavigator();
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{}} initialRouteName={"Login"}>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="NextSignupScreen"
-          component={NextSignupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="DrawerNavigator"
-          component={DrawerNavigator}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StackNavigator />
+    </>
   );
 }
