b427759fe616ad44ddfceb69cb523fa89a511a74
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 14:24:45 +0200

    Frontend: Adding login and signup screen in Stack Navigator

--- onYourWay/app/navigation/StackNavigator/StackNavigator.js
+++ onYourWay/app/navigation/StackNavigator/StackNavigator.js
@@ -1,9 +1,35 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-function StackNavigator() { 
 
-  return <NavigationContainer></NavigationContainer>;
+import Login from "../../screens/LoginScreen/Login";
+import NextSignupScreen from "../../screens/NextSignupScreen/NextSignupScreen";
+import Signup from "../../screens/SignupScreen/Signup";
+
+function StackNavigator() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{}} initialRouteName={"Login"}>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NextSignupScreen"
+          component={NextSignupScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default StackNavigator;
