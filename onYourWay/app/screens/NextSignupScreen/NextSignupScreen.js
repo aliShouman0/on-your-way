f32790dbc4dc7acdca8f283182f90f550420bc55
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 13:06:26 +0200

    Frontend: Changing the way of uploading images to be in different custom components to make code more DRY

--- onYourWay/app/screens/NextSignupScreen/NextSignupScreen.js
+++ onYourWay/app/screens/NextSignupScreen/NextSignupScreen.js
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from "react";
-import RBSheet from "react-native-raw-bottom-sheet";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Image,
   SafeAreaView,
@@ -7,112 +6,52 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-
-import {
-  MaterialCommunityIcons,
-  MaterialIcons,
-  FontAwesome,
-  Entypo,
-} from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 import AppButton from "../../components/AppButton/AppButton";
 import styles from "./styles";
 import Navbar from "../../components/Navbar/Navbar";
-import { StackActions } from "@react-navigation/native";
+import UploadImage from "../../components/UploadImage/UploadImage";
 
 function NextSignupScreen({ navigation }) {
-  const refRBSheet = useRef();
+  const uploadRBSheet = useRef();
+  const [image, setImage] = useState(null);
   const [photo, setPhoto] = useState(null);
   const [frontId, setFrontId] = useState(null);
   const [backId, setBackId] = useState(null);
   const [imageFor, setImageFor] = useState("");
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.cancelled) {
-      return result;
-    }
-    return;
-  };
-
-  const camera = async () => {
-    const permission = await ImagePicker.requestCameraPermissionsAsync();
-    if (permission.granted == false) {
-      alert(
-        "Camera permission denied Go to settings and allow it to use the camera"
-      );
-      return;
-    }
-    const result = await ImagePicker.launchCameraAsync();
-    if (!result.cancelled) {
-      return result;
-    }
-    return;
-  };
-
-  const getImage = async (by) => {
-    let photo;
-    if (by === "camera") {
-      photo = await camera();
-    }
-    if (by === "library") {
-      photo = await pickImage();
-    }
-    if (photo) {
+  const saveImage = async () => {
+    if (image) {
       switch (imageFor) {
         case "photo":
-          setPhoto(photo.uri);
+          setPhoto(image.uri);
           break;
         case "frontId":
-          setFrontId(photo.uri);
+          setFrontId(image.uri);
 
           break;
         case "backId":
-          setBackId(photo.uri);
-
+          setBackId(image.uri);
           break;
         default:
           break;
       }
     }
-    console.log(photo.uri);
-    refRBSheet.current.close();
   };
-
+  useEffect(() => {
+    saveImage();
+  }, [image]);
   return (
     <SafeAreaView style={styles.mainView}>
-      <RBSheet
-        ref={refRBSheet}
-        closeOnDragDown={true}
-        closeOnPressMask={true}
-        animationType={"fade"}
-        height={150}
-      >
-        <View style={styles.sheetView}>
-          <TouchableOpacity onPress={() => getImage("camera")}>
-            <FontAwesome name="camera" size={45} style={styles.iconSheet} />
-            <Text style={styles.sheetText}>Camera</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => getImage("library")}>
-            <Entypo name="folder-images" size={45} style={styles.iconSheet} />
-            <Text style={styles.sheetText}>Image Library</Text>
-          </TouchableOpacity>
-        </View>
-      </RBSheet>
+      <UploadImage refRBSheet={uploadRBSheet} setImage={setImage} />
       <Navbar type={"register"} title={"Register"} navigation={navigation} />
       <View style={styles.view}>
         <Text style={styles.text}>Upload your profile photo</Text>
         <TouchableOpacity
           onPress={() => {
             setImageFor("photo");
-            refRBSheet.current.open();
+            uploadRBSheet.current.open();
           }}
         >
           <View style={styles.icon}>
@@ -135,7 +74,7 @@ function NextSignupScreen({ navigation }) {
         <TouchableOpacity
           onPress={() => {
             setImageFor("frontId");
-            refRBSheet.current.open();
+            uploadRBSheet.current.open();
           }}
         >
           <View style={styles.icon}>
@@ -152,7 +91,7 @@ function NextSignupScreen({ navigation }) {
         <TouchableOpacity
           onPress={() => {
             setImageFor("backId");
-            refRBSheet.current.open();
+            uploadRBSheet.current.open();
           }}
         >
           <View style={styles.icon}>
