ebec8da942240bdb3ad250a7a92f26ca0c7dd5de
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 00:42:08 +0200

    Frontend: Adding NavBar  and main section in History screen

--- onYourWay/app/screens/History/History.js
+++ onYourWay/app/screens/History/History.js
@@ -1,6 +1,15 @@
-import React from "react"; 
-function History({ navigation }) {
-  return <></>;
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
+
+function History({navigation}) {
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <Navbar type={"main"} title={"History"} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
 export default History;
