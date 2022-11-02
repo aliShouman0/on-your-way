f32925d61b7fc786475bb8a1ea491d418bc4a31f
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 09:42:05 +0200

    Frontend: Styling the bottom sheet in the Upload Image component

--- onYourWay/app/components/UploadImage/styles.js
+++ onYourWay/app/components/UploadImage/styles.js
@@ -1,5 +1,12 @@
-import { StyleSheet } from "react-native";
+import {   StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  sheetView: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  } 
+});
