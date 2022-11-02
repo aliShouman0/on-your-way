3ec11f738133a85ed723cf25dc1291f99b5231e1
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 14:23:19 +0200

    Frontend:Creating Stack Navigator file that will be the main navigation before login

---
+++ onYourWay/app/navigation/StackNavigator/StackNavigator.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+function StackNavigator() { 
+
+  return <NavigationContainer></NavigationContainer>;
+}
+
+export default StackNavigator;
