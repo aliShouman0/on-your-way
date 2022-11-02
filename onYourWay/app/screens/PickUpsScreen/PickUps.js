ff32e8c7cfb38cbbccc06220839d5e87d0dc4fd7
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 17:50:07 +0200

    Frontend: Creating pickups screen

---
+++ onYourWay/app/screens/PickUpsScreen/PickUps.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+
+function PickUps({ navigation }) {
+  return (
+    <SafeAreaView style={styles.mainView}> 
+    </SafeAreaView>
+  );
+}
+
+export default PickUps;
