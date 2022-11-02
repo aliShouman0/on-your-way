09fddc95f8c84ad5fd0f93c7ffa0eca4cb74c6ea
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 20:45:55 +0200

    Frontend: Creating DropDown City custom component to choose the City from and to

---
+++ onYourWay/app/components/DropDownCity/DropDownCity.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker"; 
+
+function DropDownCity({ placeholder, setValue, value }) { 
+  return (
+     <></>
+  );
+}
+
+export default DropDownCity;
