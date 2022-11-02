a30fa6e2f6acdfff9a2d443f2e6c256bebdbbbfe
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-02 20:56:23 +0200

    Frontend: Styling DropDown City component  container and   text Style

--- onYourWay/app/components/DropDownCity/styles.js
+++ onYourWay/app/components/DropDownCity/styles.js
@@ -2,4 +2,26 @@ import { Platform, StatusBar, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  textStyle: {
+    color: colors.white,
+    fontWeight: "500",
+  },
+  style: {
+    backgroundColor: colors.dark,
+    borderRadius: 5,
+    borderWidth: 0,
+  },
+  dropDownContainerStyle: {
+    backgroundColor: colors.dark,
+    borderRadius: 0,
+    borderWidth: 0,
+  },
+  containerStyle: {
+    width: "35%",
+  },
+  searchTextInputStyle: {
+    borderColor: colors.white,
+    color: colors.white,
+  },
+});
