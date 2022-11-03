03ecd55aeea8a82b7acb613115ff6d69dc60efb7
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 08:49:22 +0200

    Frontend:Adding main view and  Navbar

--- onYourWay/app/screens/ChatScreen/Chat.js
+++ onYourWay/app/screens/ChatScreen/Chat.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
 
-function Chat() {
-  return <></>;
+function Chat({ navigation }) {
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
 export default Chat;
