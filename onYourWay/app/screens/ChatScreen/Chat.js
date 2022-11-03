2a28bfbb3dc9e31bc8195c20a5f622e77f260ee0
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 08:44:05 +0200

    Frontend:Creating Chat screen

---
+++ onYourWay/app/screens/ChatScreen/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Chat() {
+  return <></>;
+}
+
+export default Chat;
