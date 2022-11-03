fa5d766f33fc0cc4925e67acaff44e353f4afac5
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 10:17:53 +0200

    Frontend: Adding Flatlist and render ChatBox in Chat screen

--- onYourWay/app/screens/ChatScreen/Chat.js
+++ onYourWay/app/screens/ChatScreen/Chat.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView } from "react-native";
+import ChatBox from "../../components/ChatBox/ChatBox";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
 function Chat({ navigation }) {
   return (
     <SafeAreaView style={styles.mainView}>
-      <Navbar type={"main"} title={"Add Order"} navigation={navigation} />
+      <Navbar type={"main"} title={"Chat"} navigation={navigation} />
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(data) => data.id.toString()}
+        data={data}
+        refreshing={refreshing}
+        onRefresh={refresh}
+        renderItem={({ item, index, separators }) => (
+          <ChatBox
+            name={item.name}
+            userImg={item.userImg}
+            lastMessage={item.lastMessage}
+            date={item.date}
+          />
+        )}
+      />
     </SafeAreaView>
   );
 }
