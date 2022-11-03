6d087c11f04c28feb58064290d29561a6ea95d1a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 09:05:03 +0200

    Frontend: Creating chatbox component for chat screen

---
+++ onYourWay/app/components/ChatBox/ChatBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
+
+function ChatBox() {
+  return (
+    <View style={styles.mainView}> 
+    </View>
+  );
+}
+
+export default ChatBox;
---
+++ onYourWay/app/components/ChatBox/styles.js
@@ -0,0 +1,7 @@
+import { Platform, StatusBar, StyleSheet } from "react-native";
+import colors from "../../config/colors";
+import text from "../../config/text";
+
+export default StyleSheet.create({
+  mainView: {},
+});
