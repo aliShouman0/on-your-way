766d709d0ae2e45eecd3bbf680a31433cc095ede
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 10:39:02 +0200

    Frontend: Adding refreshing state for  Flatlist  in Chat screen

--- onYourWay/app/screens/ChatScreen/Chat.js
+++ onYourWay/app/screens/ChatScreen/Chat.js
@@ -4,7 +4,23 @@ import ChatBox from "../../components/ChatBox/ChatBox";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
+ 
+
 function Chat({ navigation }) {
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState(testing);
+
+  const refresh = () => {
+    setData([
+      {
+        id: "id3",
+        name: "Refresh Done",
+        userImg: require("../../assets/user1.jpg"),
+        date: "07/8/2022",
+        lastMessage: "Refresh Done",
+      },
+    ]);
+  };
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Chat"} navigation={navigation} />
