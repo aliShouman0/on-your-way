f6524a6adf4f645d26f0da265ed8f3ad8a71725a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-03 09:42:43 +0200

    Frontend: Styling user image and text container in chat box

--- onYourWay/app/components/ChatBox/styles.js
+++ onYourWay/app/components/ChatBox/styles.js
@@ -3,5 +3,23 @@ import colors from "../../config/colors";
 import text from "../../config/text";
 
 export default StyleSheet.create({
-  mainView: {},
+  mainView: {
+    flexDirection: "row",
+    backgroundColor: colors.darker,
+    marginHorizontal: 20,
+    marginVertical:10,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  userImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  text: {
+    flex: 1,
+    marginLeft: 15,
+    justifyContent: "center",
+  }
 });
