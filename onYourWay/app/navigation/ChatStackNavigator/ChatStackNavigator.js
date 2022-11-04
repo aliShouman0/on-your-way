400861b0bfb266711d51f6df2476b9551c97bbe6
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 16:57:18 +0200

    Frontend:  Adding screen in ChatStackNavigator

--- onYourWay/app/navigation/ChatStackNavigator/ChatStackNavigator.js
+++ onYourWay/app/navigation/ChatStackNavigator/ChatStackNavigator.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack"; 
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Chat from "../../screens/ChatScreen/Chat";
+import InChat from "../../screens/InChatScreen/InChat";
+
 function ChatStackNavigator() {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator screenOptions={{}} initialRouteName={"Chat"}>
-   
+      <Stack.Screen
+        name="Chat"
+        component={Chat}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="InChat"
+        component={InChat}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
