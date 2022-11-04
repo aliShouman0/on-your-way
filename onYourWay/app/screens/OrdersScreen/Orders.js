40b967a4ecfb621b12673dc4f7909ca6d26fc4e5
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 00:13:19 +0200

    Frontend: Adding DropDownCity filter in the Orders screen

--- onYourWay/app/screens/OrdersScreen/Orders.js
+++ onYourWay/app/screens/OrdersScreen/Orders.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
+import DropDownCity from "../../components/DropDownCity/DropDownCity";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
 function Orders({ navigation }) {
   return (
     <SafeAreaView style={styles.mainView}>
-      <Navbar type={"main"} title={"Register"} navigation={navigation} />
+      <Navbar type={"main"} title={"Register"} navigation={navigation} />{" "}
+      <View style={styles.container}>
+        <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
+        <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
+      </View>
     </SafeAreaView>
   );
 }
