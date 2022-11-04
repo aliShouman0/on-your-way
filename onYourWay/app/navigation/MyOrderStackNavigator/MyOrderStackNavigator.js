33bf3810b7c3280c2cb472c476dfa8573e7ff7a4
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 17:52:59 +0200

    Frontend: Creating My Order Stack Navigator for navigating between my order and add order screen

---
+++ onYourWay/app/navigation/MyOrderStackNavigator/MyOrderStackNavigator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack"; 
+function MyOrderStackNavigator() { 
+  return (
+    <Stack.Navigator screenOptions={{}} initialRouteName={"MyOrder"}> 
+    </Stack.Navigator>
+  );
+}
+
+export default MyOrderStackNavigator;
