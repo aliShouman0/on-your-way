bd4e2f9eb85807cd950cf2865520242d259ff22f
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 22:58:49 +0200

    Frontend: Adding user image and states for input

--- onYourWay/app/screens/AccountScreen/Account.js
+++ onYourWay/app/screens/AccountScreen/Account.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-
-import Navbar from "../../components/Navbar/Navbar";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, Image } from "react-native";
 import styles from "./styles";
+import Navbar from "../../components/Navbar/Navbar";
+import Rate from "../../components/Rate/Rate";
+import text from "../../config/text";
 
 function Account({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [date, setDate] = useState(new Date());
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Account"} navigation={navigation} />
+      <Image
+        resizeMode="contain"
+        source={require("../../assets/user1.jpg")}
+        style={styles.userImg}
+      />
+      <Rate rate={3} size={30} styleText={{ fontSize: text.sizeMid }} />
+      <ScrollView style={styles.scroll}></ScrollView>
     </SafeAreaView>
   );
 }
