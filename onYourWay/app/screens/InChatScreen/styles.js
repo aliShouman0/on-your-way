31d82d033c296f742e1ca808818d5f97f9b40ff8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 16:44:49 +0200

    Frontend: Styling main view section and user images

--- onYourWay/app/screens/InChatScreen/styles.js
+++ onYourWay/app/screens/InChatScreen/styles.js
@@ -1,2 +1,18 @@
 import { Platform, StatusBar, StyleSheet } from "react-native";
-export default StyleSheet.create({});
+import colors from "../../config/colors";
+import text from "../../config/text";
+
+export default StyleSheet.create({
+  mainView: {
+    backgroundColor: colors.primary,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    height: "100%",
+  },
+  userImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+ 
+});
