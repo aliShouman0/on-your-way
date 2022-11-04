0aeaf92cc857a6f917b37e92a006d2c5b9376ed9
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 23:01:07 +0200

    Frontend: Adding Inputs of user info in Account screen

--- onYourWay/app/screens/AccountScreen/Account.js
+++ onYourWay/app/screens/AccountScreen/Account.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import { SafeAreaView, ScrollView, Image } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import AppButton from "../../components/AppButton/AppButton";
 import styles from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import Input from "../../components/Input/Input";
 import Navbar from "../../components/Navbar/Navbar";
 import Rate from "../../components/Rate/Rate";
 import text from "../../config/text";
@@ -20,7 +30,32 @@ function Account({ navigation }) {
         style={styles.userImg}
       />
       <Rate rate={3} size={30} styleText={{ fontSize: text.sizeMid }} />
-      <ScrollView style={styles.scroll}></ScrollView>
+      <ScrollView style={styles.scroll}>
+        <View style={styles.inputContainer}>
+          <Input
+            text="Full Name"
+            value={name}
+            setValue={setName}
+            editable={false}
+            contextMenuHidden={true}
+          />
+          <TouchableOpacity style={styles.date}>
+            <Input
+              text="Date Of Birth"
+              value={date.toDateString()}
+              placeholder={date.toDateString()}
+              editable={false}
+              contextMenuHidden={true}
+            />
+            <MaterialIcons
+              style={styles.datePic}
+              name="date-range"
+              size={35}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
