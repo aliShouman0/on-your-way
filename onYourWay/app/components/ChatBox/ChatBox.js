412ec0289d9bea1ba8f122e5af7ff8602958940e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 15:04:21 +0200

    Frontend: Adding Navigation for the chatbox component to navigate to InChat Screen

--- onYourWay/app/components/ChatBox/ChatBox.js
+++ onYourWay/app/components/ChatBox/ChatBox.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import styles from "./styles";
 
-function ChatBox({ name, userImg, lastMessage, date, onPress, navigation }) {
+function ChatBox({ name, userImg, lastMessage, date, navigation }) {
   return (
-    <View style={styles.mainView}>
-      <Image resizeMode="contain" source={userImg} style={styles.userImg} />
-      <View style={styles.text}>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.lastMessage}>{lastMessage}</Text>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("InChat", { userName: name, userImg });
+      }}
+    >
+      <View style={styles.mainView}>
+        <Image resizeMode="contain" source={userImg} style={styles.userImg} />
+        <View style={styles.text}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.lastMessage}>{lastMessage}</Text>
+        </View>
+        <Text style={styles.date}>{date}</Text>
       </View>
-      <Text style={styles.date}>{date}</Text>
-    </View>
+    </TouchableOpacity>
   );
 }
 
