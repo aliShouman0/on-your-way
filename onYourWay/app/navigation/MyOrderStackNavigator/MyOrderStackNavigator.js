d89a5d3c65657592b232f916cd4eabca50a57d80
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 17:55:48 +0200

    Frontend: Adding screens to stack in MyOrderStackNavigator

--- onYourWay/app/navigation/MyOrderStackNavigator/MyOrderStackNavigator.js
+++ onYourWay/app/navigation/MyOrderStackNavigator/MyOrderStackNavigator.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack"; 
-function MyOrderStackNavigator() { 
+import { createStackNavigator } from "@react-navigation/stack";
+import MyOrder from "../../screens/MyOrderScreen/MyOrder";
+import AddOrder from "../../screens/AddOrderScreen/AddOrder";
+
+function MyOrderStackNavigator() {
+  const Stack = createStackNavigator();
+
   return (
-    <Stack.Navigator screenOptions={{}} initialRouteName={"MyOrder"}> 
+    <Stack.Navigator initialRouteName={"MyOrder"}>
+      <Stack.Screen
+        name="MyOrder"
+        component={MyOrder}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="AddOrder"
+        component={AddOrder}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
