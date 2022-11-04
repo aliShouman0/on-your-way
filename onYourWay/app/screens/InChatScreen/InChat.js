a6619cdf26c4a9771bfbdd05e70681a2a30f3f37
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 16:35:41 +0200

    Frontend: Adding main ScrollView section for  messages in the InChat screen

--- onYourWay/app/screens/InChatScreen/InChat.js
+++ onYourWay/app/screens/InChatScreen/InChat.js
@@ -15,6 +15,20 @@ function InChat({ navigation, route }) {
           <Image resizeMode="contain" source={userImg} style={styles.userImg} />
         }
       />
+      <ScrollView style={styles.message}>
+        <Text style={styles.getMessage}>Hi</Text>
+        <Text style={styles.sendMessage}>Hello</Text>
+        <Text style={styles.getMessage}>
+          So what if you pay 10$ and get by 1h
+        </Text>
+        <Text style={styles.sendMessage}>
+          Sounds Good but with live location
+        </Text>
+        <Text style={styles.getMessage}>Okay Deal</Text>
+        <Text style={styles.sendMessage}>Hello</Text>
+        <Text style={styles.getMessage}>Hi</Text>
+        <Text style={styles.sendMessage}>Hello</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
