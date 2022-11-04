ddc49cc273d1c3e1b6c8daa68b3361333d98cc52
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 18:20:53 +0200

    Frontend: Adding Text input and its state for messages in InChat screen

--- onYourWay/app/screens/InChatScreen/InChat.js
+++ onYourWay/app/screens/InChatScreen/InChat.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { Image, SafeAreaView, ScrollView, Text } from "react-native";
+import React, { useState } from "react";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+
+import Input from "../../components/Input/Input";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
 function InChat({ navigation, route }) {
+  const [message, setMessage] = useState("");
   const { userName, userImg } = route.params;
   return (
     <SafeAreaView style={styles.mainView}>
@@ -29,6 +32,15 @@ function InChat({ navigation, route }) {
         <Text style={styles.getMessage}>Hi</Text>
         <Text style={styles.sendMessage}>Hello</Text>
       </ScrollView>
+
+      <View>
+        <Input 
+          placeholder={"Say Hi!"}
+          value={message}
+          setValue={setMessage}
+          style={styles.input}
+        />
+      </View>
     </SafeAreaView>
   );
 }
