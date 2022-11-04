1e43c398cb3810e2a6b8d52cacddc0599b375d21
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-04 22:31:03 +0200

    Frontend: Adding and styling the navbar

--- onYourWay/app/screens/AccountScreen/Account.js
+++ onYourWay/app/screens/AccountScreen/Account.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
+
+import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
-function Account() {
-  return <SafeAreaView style={styles.mainView}></SafeAreaView>;
+function Account({ navigation }) {
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <Navbar type={"main"} title={"Account"} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
 export default Account;
