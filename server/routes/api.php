457cac97579f50d71e993838b8900bac8ce26076
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 22:32:29 +0200

    Backend: Creating login api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "auth"], function () {
+   
+    Route::post('login', [AuthController::class, 'login'])->name("login");
+    Route::group(["middleware" => "security"], function () {
+        // get my data
+        Route::post('me', [AuthController::class, 'me']);
+    });
 });
+ 
+ 
