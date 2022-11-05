a88a75d7c410f023d21d6a1ec1e85132f2f785d3
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 16:24:44 +0200

    Backend: Creating users table/ migrations

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,34 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+     
     public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+    {  Schema::create('user_types', function (Blueprint $table) {
+        $table->id();
+        $table->string("role");
+        $table->timestamps();
+    });
+    Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string("name");
+        $table->string("birthday");
+        $table->string("email");
+        $table->string("phone");
+        $table->string("address");
+        $table->string("password");
+        $table->string("avatar");
+        $table->string("front_id_photo");
+        $table->string("back_id_photo");
+        $table->boolean("is_verified");
+        $table->integer("rate");
+        $table->integer("user_type_id")->references("id")->on("user_types");
+        $table->timestamps();
+    });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+ 
     public function down()
     {
+        Schema::dropIfExists('user_types');
         Schema::dropIfExists('users');
     }
 };
