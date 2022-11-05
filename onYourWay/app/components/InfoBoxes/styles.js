dad1570c5867fc6ba7daf500af13ed5157e64b55
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 11:51:12 +0200

    Frontend: Styling main section and container to be as boxes

--- onYourWay/app/components/InfoBoxes/styles.js
+++ onYourWay/app/components/InfoBoxes/styles.js
@@ -2,4 +2,19 @@ import { StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  orderDetails: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 12,
+  },
+  container: {
+    borderRadius: 10,
+    borderColor: colors.lightWhite,
+    borderStyle: "solid",
+    borderWidth: 0.5,
+    width: "30%",
+    height: 55,
+    padding: 5,
+  }
+});
