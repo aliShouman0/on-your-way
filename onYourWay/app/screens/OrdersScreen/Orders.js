ca276e5083e293d11dda4621983f7bd10810e088
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 09:59:19 +0200

    Frontend: Adding states for DropDownCity in the Orders screen

--- onYourWay/app/screens/OrdersScreen/Orders.js
+++ onYourWay/app/screens/OrdersScreen/Orders.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, View } from "react-native";
 
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
@@ -6,9 +6,11 @@ import Navbar from "../../components/Navbar/Navbar";
 import styles from "./styles";
 
 function Orders({ navigation }) {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
   return (
     <SafeAreaView style={styles.mainView}>
-      <Navbar type={"main"} title={"Register"} navigation={navigation} />{" "}
+      <Navbar type={"main"} title={"Register"} navigation={navigation} />
       <View style={styles.container}>
         <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
         <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
