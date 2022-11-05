f73c48fb4fb9c57367a58ac50dd18e3cb3b5e0ad
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:31:26 +0200

    Frontend: Adding render item to Flatlist to render NewOrderInfo components

--- onYourWay/app/screens/OrdersScreen/Orders.js
+++ onYourWay/app/screens/OrdersScreen/Orders.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
 
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
-import Navbar from "../../components/Navbar/Navbar"; 
+import Navbar from "../../components/Navbar/Navbar";
+import NewOrderInfo from "../../components/NewOrderInfo/NewOrderInfo";
 import styles from "./styles";
 
  
@@ -25,7 +26,21 @@ function Orders({ navigation }) {
         data={data}
         refreshing={refreshing}
         onRefresh={refresh}
-       
+        renderItem={({ item, index, separators }) => (
+          <NewOrderInfo
+            key={item.id}
+            id={item.id}
+            userName={item.userName}
+            userImg={item.userImg}
+            from={item.from}
+            to={item.to}
+            pay={item.pay}
+            orderImg1={item.orderImg1}
+            orderImg2={item.orderImg2}
+            orderImg3={item.orderImg3}
+            orderDescription={item.orderDescription}
+          />
+        )}
       />
     </SafeAreaView>
   );
