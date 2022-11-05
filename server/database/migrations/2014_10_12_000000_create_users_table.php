38ea093d68b4132c55a2eb8a8bd8dfd4cf6c58f0
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 23:58:05 +0200

    Backend: Editing user migration adding default values

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-     
+
     public function up()
-    {  Schema::create('user_types', function (Blueprint $table) {
-        $table->id();
-        $table->string("role");
-        $table->timestamps();
-    });
-    Schema::create('users', function (Blueprint $table) {
-        $table->id();
-        $table->string("name");
-        $table->string("birthday");
-        $table->string("email");
-        $table->string("phone");
-        $table->string("address");
-        $table->string("password");
-        $table->string("avatar");
-        $table->string("front_id_photo");
-        $table->string("back_id_photo");
-        $table->boolean("is_verified");
-        $table->integer("rate");
-        $table->integer("user_type_id")->references("id")->on("user_types");
-        $table->timestamps();
-    });
+    {
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("role");
+            $table->timestamps();
+        });
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("birthday");
+            $table->string("email");
+            $table->string("phone");
+            $table->string("address");
+            $table->string("password");
+            $table->string("avatar");
+            $table->string("front_id_photo");
+            $table->string("back_id_photo");
+            $table->boolean("is_verified")->default(0);
+            $table->integer("rate")->default(0);
+            $table->integer("user_type_id")->references("id")->on("user_types")->default(2);
+            $table->timestamps();
+        });
     }
- 
+
     public function down()
     {
         Schema::dropIfExists('user_types');
