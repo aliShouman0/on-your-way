a4c75c4f8c7c0b44c87071707e9aef221e542561
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:33:20 +0200

    Frontend: Create a refresh function for FlatList and add testing data

--- onYourWay/app/screens/OrdersScreen/Orders.js
+++ onYourWay/app/screens/OrdersScreen/Orders.js
@@ -6,13 +6,28 @@ import Navbar from "../../components/Navbar/Navbar";
 import NewOrderInfo from "../../components/NewOrderInfo/NewOrderInfo";
 import styles from "./styles";
 
- 
 function Orders({ navigation }) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [refreshing, setRefreshing] = useState(false);
   const [data, setData] = useState(testing);
- 
+
+  const refresh = () => {
+    setData([
+      {
+        id: "id3",
+        userName: "Refresh Done",
+        userImg: require("../../assets/user1.jpg"),
+        from: "Beirut",
+        to: "Tripoli",
+        pay: "745000L.L",
+        orderImg1: require("../../assets/keyboard.jpg"),
+        orderImg2: require("../../assets/keyboard.jpg"),
+        orderImg3: require("../../assets/keyboard.jpg"),
+        orderDescription: "Refresh Done",
+      },
+    ]);
+  };
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Orders"} navigation={navigation} />
