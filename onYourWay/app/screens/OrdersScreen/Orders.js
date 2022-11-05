02696feb792f22608b87cf6a1eebd6bfd9a5f95a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:29:42 +0200

    Frontend: Adding Flatlist with its props and states in Orders screen

--- onYourWay/app/screens/OrdersScreen/Orders.js
+++ onYourWay/app/screens/OrdersScreen/Orders.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import { FlatList, SafeAreaView, View } from "react-native";
 
 import DropDownCity from "../../components/DropDownCity/DropDownCity";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar"; 
 import styles from "./styles";
 
+ 
 function Orders({ navigation }) {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState(testing);
+ 
   return (
     <SafeAreaView style={styles.mainView}>
-      <Navbar type={"main"} title={"Register"} navigation={navigation} />
+      <Navbar type={"main"} title={"Orders"} navigation={navigation} />
       <View style={styles.container}>
         <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
         <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
       </View>
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(data) => data.id.toString()}
+        data={data}
+        refreshing={refreshing}
+        onRefresh={refresh}
+       
+      />
     </SafeAreaView>
   );
 }
