995c8ef0c8ddde88510dd84869773f3015c42c3c
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-06 00:00:47 +0200

    Backend: creating function to manage and save image base64 in user controller

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -9,5 +9,15 @@ use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-   
+    function save_images($image_base64, $Image_name, $isId)
+    {
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+        $folder = $isId ? "ids/" : "user_images/";
+        $data = base64_decode(explode(',', $image_base64)[1]);
+        $save_name = $folder . $Image_name . '.png';
+        Storage::disk('local')->put($save_name,  $data);
+    }
+
 }
