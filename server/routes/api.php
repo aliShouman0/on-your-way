fc0dd3270188e7aea9252a9020638fd77a35b667
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-06 00:05:33 +0200

    Backend: creating signup api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,16 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "auth"], function () {
-   
+Route::group(["prefix" => "ony"], function () {
+
+    // add/signup  user
+    Route::post("signup", [UserController::class, "signup"])->name("signup");
+    //login
     Route::post('login', [AuthController::class, 'login'])->name("login");
     Route::group(["middleware" => "security"], function () {
         // get my data
         Route::post('me', [AuthController::class, 'me']);
     });
 });
- 
  
