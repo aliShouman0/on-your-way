1922d831bd895f21365a434b503fc4acd9f9864c
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 17:26:13 +0200

    Backend: Creating completed_pickups table/migration

--- server/database/migrations/2022_11_05_145449_create_pickups_completed_canceled_table.php
+++ server/database/migrations/2022_11_05_145449_create_pickups_completed_canceled_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
             $table->string("reason");
             $table->timestamps();
         });
+        Schema::create('completed_pickups', function (Blueprint $table) {
+            $table->id();
+            $table->integer("pickup_id")->references("id")->on("pickups");
+            $table->string("picker_comment"); 
+            $table->string("receiver_comment"); 
+            $table->integer("picker_rated"); 
+            $table->integer("receiver_rated"); 
+            $table->string("date_time");
+            $table->timestamps();
+        });
     }
  
     public function down()
