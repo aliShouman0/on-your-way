312b268382646dc95a256aa3f8864c6c1d262cfc
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:03:01 +0200

    Frontend: Adding userInfo and order Info section for NewOrderInfo components

--- onYourWay/app/components/NewOrderInfo/NewOrderInfo.js
+++ onYourWay/app/components/NewOrderInfo/NewOrderInfo.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
-
+ 
+import InfoBoxes from "../InfoBoxes/InfoBoxes"; 
 import styles from "./styles";
 
-function NewOrderInfo({}) {
-  return <View style={styles.mainView}></View>;
+function NewOrderInfo({
+  id,
+  userName,
+  userImg,
+  from,
+  to,
+  pay,  
+}) {
+  return (
+    <View style={styles.mainView}>
+      <View style={styles.userInfo}>
+        <Image resizeMode="contain" source={userImg} style={styles.userImg} />
+        <Text style={styles.userName}>{userName}</Text>
+      </View>
+      <InfoBoxes from={from} to={to} pay={pay} /> 
+    </View>
+  );
 }
 export default NewOrderInfo;
