4aadbaeeed7b38c70784b7db20f462d3e1e9e12f
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-06 00:59:42 +0200

    Backend: Creating Order Controller

---
+++ server/app/Http/Controllers/OrderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Pickup;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    
+}
