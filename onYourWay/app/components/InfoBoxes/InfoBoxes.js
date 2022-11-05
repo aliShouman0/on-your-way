ae0e2b03f729453ed7d339313a62f16b9b7f4af8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 11:47:44 +0200

    Frontend: Creating InfoBoxes component to use the same style for order info

---
+++ onYourWay/app/components/InfoBoxes/InfoBoxes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+function InfoBoxes({from,to,pay}) {
+  return (
+    <View style={styles.orderDetails}> 
+    </View>
+  );
+}
+
+export default InfoBoxes;
