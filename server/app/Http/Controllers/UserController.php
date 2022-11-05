6749e17b03c86da9caaff919a1ec3cdcc535c4bc
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 23:59:10 +0200

    Backend: Creating user controller

---
+++ server/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+   
+}
