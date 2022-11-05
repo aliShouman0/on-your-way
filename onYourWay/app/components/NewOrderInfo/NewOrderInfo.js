636c142df05ed8e421380043f752ee85b5da99e2
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:00:47 +0200

    Frontend: Creating NewOrderInfo components for the Orders screen

---
+++ onYourWay/app/components/NewOrderInfo/NewOrderInfo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+import styles from "./styles";
+
+function NewOrderInfo({}) {
+  return <View style={styles.mainView}></View>;
+}
+export default NewOrderInfo;
