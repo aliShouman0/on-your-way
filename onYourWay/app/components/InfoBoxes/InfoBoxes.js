82c9f6f1912554bdf09ed1cd507b89a64772b508
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 11:49:04 +0200

    Frontend: Adding the 3 main boxes / section in InfoBoxes

--- onYourWay/app/components/InfoBoxes/InfoBoxes.js
+++ onYourWay/app/components/InfoBoxes/InfoBoxes.js
@@ -4,7 +4,19 @@ import styles from "./styles";
 
 function InfoBoxes({from,to,pay}) {
   return (
-    <View style={styles.orderDetails}> 
+    <View style={styles.orderDetails}>
+      <View style={styles.container}>
+        <Text style={styles.key}>From</Text>
+        <Text style={styles.value}>{from}</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.key}>To</Text>
+        <Text style={styles.value}>{to}</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.key}>Pay</Text>
+        <Text style={styles.value}>{pay}</Text>
+      </View>
     </View>
   );
 }
