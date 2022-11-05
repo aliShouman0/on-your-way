eaa63061fefe29e4991febec1ca811b85fd962b5
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-05 12:13:55 +0200

    Frontend: Styling main View and userInfo section

--- onYourWay/app/components/NewOrderInfo/styles.js
+++ onYourWay/app/components/NewOrderInfo/styles.js
@@ -2,4 +2,20 @@ import { StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import text from "../../config/text";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  mainView: {
+    width: "90%",
+    height: 420,
+    backgroundColor: colors.darker,
+    borderRadius: 10,
+    marginVertical: 10,
+    padding: 15,
+    alignSelf: "center",
+  },
+  userInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+ 
+});
