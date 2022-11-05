24695ba4bbde03dc70e117ae9a344bfb3b8a2733
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-06 01:06:14 +0200

    Backend: Creating get_my_order  api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,9 +12,11 @@ Route::group(["prefix" => "ony"], function () {
     Route::post("signup", [UserController::class, "signup"])->name("signup");
     //login
     Route::post('login', [AuthController::class, 'login'])->name("login");
+    //JWT auth  
     Route::group(["middleware" => "security"], function () {
-        // get my data
+        // get my data/current user info
         Route::post('me', [AuthController::class, 'me']);
+        //get my order
+        Route::get('get_my_order', [OrderController::class, 'getMyOrder']);
     });
 });
- 
