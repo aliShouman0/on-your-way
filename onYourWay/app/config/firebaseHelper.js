7845837b8ddb788b1975ee386694084d9f5de2e5
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 01:25:09 +0200

    Frontend: Editing on Add Friend function if Friend does not exist before in the friend list, creating a chat room and store the chat room id for messaging

--- onYourWay/app/config/firebaseHelper.js
+++ onYourWay/app/config/firebaseHelper.js
@@ -52,11 +52,21 @@ const onAddFriend = async (phone, myData, setIsLoading) => {
         // don't let user add a user twice
         setIsLoading(false);
         return;
-      }  
+      }
+      // create a chatroom and store the chatroom id
+      const newChatroomRef = push(ref(database, "chatrooms"), {
+        firstUser: myData.phone,
+        secondUser: user.phone,
+        messages: [],
+      });
+      const newChatroomId = newChatroomRef.key;
+      const userFriends = user.friends || [];
+      
     }
   } catch (error) {
     console.error(error);
   }
   setIsLoading(false);
 };
+
 export default { onAddFriend, findUser, chatLogin };
