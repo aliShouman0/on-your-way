1a597aa4072d7bffa194719cabef2c3e9763cc9e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 01:11:32 +0200

    Frontend: Creating findUser function  that will get user data from firebase if this user exists

--- onYourWay/app/config/firebaseHelper.js
+++ onYourWay/app/config/firebaseHelper.js
@@ -7,3 +7,12 @@ import {
   update,
 } from "firebase/database";
 
+
+const findUser = async (phone) => {
+  const database = getDatabase();
+  const mySnapshot = await get(ref(database, `users/${phone}`));
+  return mySnapshot.val();
+};
+
+
+
