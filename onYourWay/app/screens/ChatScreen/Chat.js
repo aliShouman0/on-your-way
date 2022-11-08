0e5431a205f41dbc3584e39e72d8ef3b150e32b0
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 01:38:28 +0200

    Frontend: Adding states for users/friends  for firebase and removing the testing data

--- onYourWay/app/screens/ChatScreen/Chat.js
+++ onYourWay/app/screens/ChatScreen/Chat.js
@@ -1,25 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, SafeAreaView } from "react-native";
+import firebaseHelper from "../../config/firebaseHelper";
 import ChatBox from "../../components/ChatBox/ChatBox";
 import Navbar from "../../components/Navbar/Navbar";
+import Loading from "../../components/Loading/Loading";
 import styles from "./styles";
 
- 
-function Chat({ navigation }) {
-  const [refreshing, setRefreshing] = useState(false);
-  const [data, setData] = useState(testing);
+function Chat({ navigation, phone = 71993980 }) {
+  const [data, setData] = useState(renderedUser);
+  const [users, setUsers] = useState([]);
+  const [myData, setMyData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const renderedUser = [];
 
-  const refresh = () => {
-    setData([
-      {
-        id: "id3",
-        name: "Refresh Done",
-        userImg: require("../../assets/user1.jpg"),
-        date: "07/8/2022",
-        lastMessage: "Refresh Done",
-      },
-    ]);
-  };
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"Chat"} navigation={navigation} />
@@ -27,8 +20,6 @@ function Chat({ navigation }) {
         style={styles.flatList}
         keyExtractor={(data) => data.id.toString()}
         data={data}
-        refreshing={refreshing}
-        onRefresh={refresh}
         renderItem={({ item, index, separators }) => (
           <ChatBox
             name={item.name}
@@ -36,6 +27,8 @@ function Chat({ navigation }) {
             lastMessage={item.lastMessage}
             date={item.date}
             navigation={navigation}
+            myData={myData}
+            user={item.user}
           />
         )}
       />
