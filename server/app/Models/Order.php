dedbaedb2203ab67377f30a7c1d06a52379258bd
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 19:33:47 +0200

    Backend: Creating join/belongsTo functions for Order table

--- server/app/Models/Order.php
+++ server/app/Models/Order.php
@@ -8,5 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    
+    public function PickupInfo()
+    {
+        return $this->belongsTo(Pickup::class, "id", "order_id")->with("pickerInfo");
+    }
+    public function EndedPickupInfo()
+    {
+        return $this->belongsTo(Pickup::class, "id", "order_id")->with("completedPickupInfo")->with("canceledPickupInfo"); 
+    }
+    public function userInfo()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
 }
