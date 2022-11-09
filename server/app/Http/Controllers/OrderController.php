97747552cbf52013ce0afa6fe953b4f2a699de8a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 20:39:38 +0200

    Backend: Creating get Pickup function that will get all uncompleted Pickup for the current login user in parallel to the order Info

--- server/app/Http/Controllers/OrderController.php
+++ server/app/Http/Controllers/OrderController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CanceledPickup;
+use App\Models\CompletedPickup;
 use App\Models\Order;
 use App\Models\Pickup;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class OrderController extends Controller
 {
@@ -13,7 +17,7 @@ class OrderController extends Controller
     function getMyOrder()
     {
         $id = Auth::id();
-        $order = Order::where("user_id", $id)->where("ended", false)->get();
+        $order = Order::where("user_id", $id)->where("ended", false)->with("PickupInfo")->get();
         if ($order) {
 
             return response()->json([
@@ -27,4 +31,25 @@ class OrderController extends Controller
             "data" => "Error -Some Thing went wrong "
         ], 400);
     }
+
+    // get pickup
+    function getMyPickup()
+    {
+        $id = Auth::id();
+        $pickup = Pickup::where("picker_id", $id)->where("completed", false)->where("canceled", false)->with("orderInfo")->get();
+        if ($pickup) {
+
+            return response()->json([
+                "status" => 1,
+                "data" => $pickup,
+                "refresh" => Auth::refresh()
+            ]);
+        }
+        return response()->json([
+            "status" => 0,
+            "data" => "Error -Some Thing went wrong "
+        ], 400);
+    }
+
+  
 }
