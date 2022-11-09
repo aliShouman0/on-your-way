702a6329702badd02ab60403ed63864fc3893239
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 19:32:19 +0200

    Backend: Creating join/belongsTo functions for Pickup table

--- server/app/Models/Pickup.php
+++ server/app/Models/Pickup.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Pickup extends Model
 {
     use HasFactory;
+
+    public function pickerInfo()
+    {
+        return $this->belongsTo(User::class, "picker_id");
+    }
+
+    public function completedPickupInfo()
+    {
+        return $this->belongsTo(CompletedPickup::class, "id", "pickup_id"); 
+    }
+
+    public function canceledPickupInfo()
+    {
+        return $this->belongsTo(CanceledPickup::class, "id", "pickup_id"); 
+    }
+
+    public function orderInfo()
+    {
+        return $this->belongsTo(Order::class, "order_id")->with("userInfo");
+    }
 }
