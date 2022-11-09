d65af1a1119a682009a1310ebca41fd3d5cab63a
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-09 19:22:35 +0200

    Backend: modifying users after migrate

---
+++ server/database/migrations/2022_11_06_205404_modify_users_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{ 
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer("profit")->default(0)->after("rate");
+        });
+    } 
+    public function down()
+    {
+        //
+    }
+};
