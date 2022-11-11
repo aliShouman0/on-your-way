a1f75046a03e78f4a998cf710ba963445faae4cd
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-11 19:59:21 +0200

    Backend: Adding/deffining Admin middleware in kernel/route Middleware

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'security' => \App\Http\Middleware\JWT::class,
+        'isAdmin' => \App\Http\Middleware\Admin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
