c886defa4d80d1371624426ffc32c24fdf40c494
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-11 19:57:31 +0200

    Backend: Adding the condition/logic to check if the current user is an admin in the Admin middleware

--- server/app/Http/Middleware/Admin.php
+++ server/app/Http/Middleware/Admin.php
@@ -4,11 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Admin
 {
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $user = Auth::user();
+        if ($user->user_type_id == 1) {
+            return $next($request);
+        } else {
+            return response()->json([
+                "status" => 0,
+                "data" => "Error -Some Thing went wrong "
+            ], 400);
+        }
     }
 }
