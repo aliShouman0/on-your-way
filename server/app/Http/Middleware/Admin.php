55bf9825f6b8715cefa16a42f6d9a755b4bcf2d0
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-11 19:53:02 +0200

    Backend: Creating Admin middleware to check if the current user is an admin

---
+++ server/app/Http/Middleware/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
