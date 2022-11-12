bf6cbe382cc242287692c27f07d6cb0ae07c2c24
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 04:47:01 +0200

    Frontend: Styling main view section/container

--- onYourWay/app/screens/LocationScreen/styles.js
+++ onYourWay/app/screens/LocationScreen/styles.js
@@ -1,4 +1,12 @@
 import { Platform, StatusBar, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  mainView: {
+    backgroundColor: colors.primary,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    alignItems: "center",
+    height: "100%",
+  } 
+});
