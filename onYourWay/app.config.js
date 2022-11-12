2f6c790fadc2f68bba78f4249609612c96050a78
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 04:08:23 +0200

    Frontend: Adding Package, and library... that needed for setting file

--- onYourWay/app.config.js
+++ onYourWay/app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "onYourWay",
     "slug": "onYourWay",
@@ -26,6 +28,15 @@
     },
     "web": {
       "favicon": "./app/assets/logo-gold.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      googleApiKey: process.env.GOOGLE_MAPS_APIKEY
     }
   }
 }
