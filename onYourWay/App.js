fe3c716e386f92082a960b716ab37999f8bf125d
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 17:48:03 +0200

    Linking: Adding react query to the application add  QueryClient and QueryClientProvider

--- onYourWay/App.js
+++ onYourWay/App.js
@@ -1,10 +1,12 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./app/navigation/StackNavigator/StackNavigator";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StackNavigator />
-    </>
+    </QueryClientProvider>
   );
 }
