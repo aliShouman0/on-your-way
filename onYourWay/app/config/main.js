78b538c399f8432fbdae6b322575d4aa5c53f253
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 15:02:56 +0200

    Linking: Creating get Api function for requesting from the server

--- onYourWay/app/config/main.js
+++ onYourWay/app/config/main.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const baseUrl = "http://192.168.8.135:8000/api/ony";
+
 const postAPI = async (api_url, api_data, api_token = null) => {
   try {
     return await axios.post(api_url, api_data, {
@@ -14,4 +15,19 @@ const postAPI = async (api_url, api_data, api_token = null) => {
     return error;
   }
 };
- 
+
+const getAPI = async (api_url, api_token) => {
+  try {
+    return await axios(api_url, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: "token " + token,
+      },
+    });
+  } catch (error) {
+    console.log("Error from get API ", error);
+    console.log("error.response", error.response);
+    return error;
+  }
+};
+
