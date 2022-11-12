d57f2c4127967fae1156828d0bbc9711b2e66bc4
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 19:39:39 +0200

    Linking: Adding login function using useMutation in react query and creating onLogin function  to handle login  event

--- onYourWay/app/screens/LoginScreen/Login.js
+++ onYourWay/app/screens/LoginScreen/Login.js
@@ -6,15 +6,37 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
+import { useMutation } from "react-query";
+import Toast from "react-native-root-toast";
 
 import AppButton from "../../components/AppButton/AppButton";
 import Input from "../../components/Input/Input";
 import styles from "./styles";
+import main from "../../config/main";
+import Loading from "../../components/Loading/Loading";
 
 function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const {
+    mutate: login,
+    isError,
+    isLoading,
+    error: loginUpError,
+    status,
+    data: result,
+  } = useMutation((user) => main.login(user));
 
+  const onLogin = async () => {
+    if (!email || !password) {
+      alert("All Image Are Required");
+      return;
+    }
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <SafeAreaView style={styles.mainView}>
       <Image
@@ -36,15 +58,7 @@ function Login({ navigation }) {
           setValue={setPassword}
           secureTextEntry={true}
         />
-        <AppButton
-          value={"Login"}
-          onPress={() =>
-            navigation.reset({
-              index: 0,
-              routes: [{ name: "DrawerNavigator" }],
-            })
-          }
-        />
+        <AppButton value={"Login"} onPress={onLogin} />
         <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
           <Text style={styles.signup}>
             Don't have an account? <Text style={styles.register}>Register</Text>
