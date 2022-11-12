0d6f22f0bb66e143fecf048ce230164f6f6daeb8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 04:45:53 +0200

    Frontend: Adding Navbar and Creating a function to get location if permission is granted

--- onYourWay/app/screens/LocationScreen/Location.js
+++ onYourWay/app/screens/LocationScreen/Location.js
@@ -1,7 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./styles";
+import * as geoLocation from "expo-location";
+import Loading from "../../components/Loading/Loading";
+
 function Location({ navigation }) {
-  return <SafeAreaView style={styles.mainView}></SafeAreaView>;
+  const [load, setLoad] = useState(false);
+  const [myLocation, setMyLocation] = useState({
+    latitude: 33.450736,
+    longitude: 35.396315,
+  });
+
+  useEffect(() => {
+    setLoad(true);
+    const getLocation = async () => {
+      let { status } = await geoLocation.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+      let getLocation = await geoLocation.getCurrentPositionAsync({});
+      //  console.log("my ", getLocation);
+      setMyLocation({
+        latitude: getLocation.coords.latitude,
+        longitude: getLocation.coords.longitude,
+      });
+      setLoad(false);
+    };
+    getLocation();
+  }, []);
+
+  if (load) {
+    return <Loading />;
+  }
+
+  return (
+    <SafeAreaView style={styles.mainView}>
+      <Navbar type={"back"} title={"Location"} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
 export default Location;
