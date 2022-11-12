a6e9c96353fad8239842e2a8935da077d7da0552
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-12 18:42:29 +0200

    Frontend: Adding Cross platform toast messages

--- onYourWay/App.js
+++ onYourWay/App.js
@@ -1,12 +1,15 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./app/navigation/StackNavigator/StackNavigator";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RootSiblingParent } from "react-native-root-siblings";
 
 export default function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <StackNavigator />
-    </QueryClientProvider>
+    <RootSiblingParent>
+      <QueryClientProvider client={queryClient}>
+        <StackNavigator />
+      </QueryClientProvider>
+    </RootSiblingParent>
   );
 }
