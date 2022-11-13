db1b7037470582a1fe306c0cfcc3b836ae2247f8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-13 22:59:57 +0200

    Linking: Passing new props for components and Fixing loading handling by adding conditional render when is loading

--- onYourWay/app/components/OrderStatus/OrderStatus.js
+++ onYourWay/app/components/OrderStatus/OrderStatus.js
@@ -4,6 +4,7 @@ import RBSheet from "react-native-raw-bottom-sheet";
 import DropDownPicker from "react-native-dropdown-picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import Toast from "react-native-root-toast";
+import { useQuery } from "react-query";
 
 import colors from "../../config/colors";
 import SmallButton from "../SmallButton/SmallButton";
@@ -15,9 +16,15 @@ import ReceiveOrder from "../ReceiveOrder/ReceiveOrder";
 import AppButton from "../AppButton/AppButton";
 import main from "../../config/main";
 import Loading from "../Loading/Loading";
-import { useQuery } from "react-query";
 
-function OrderStatus({ refRBSheet, isReceiver, navigation, id }) {
+function OrderStatus({
+  refRBSheet,
+  isReceiver,
+  navigation,
+  setRefreshing,
+  id,
+  pickupId,
+}) {
   const cancelOrderBSheet = useRef();
   const receiveOrderBSheet = useRef();
   const windowHeight = Dimensions.get("window").height;
@@ -64,10 +71,7 @@ function OrderStatus({ refRBSheet, isReceiver, navigation, id }) {
       }
     }
   }, [result]);
-  
-  if (isLoading) {
-    return <Loading />;
-  }
+
   return (
     <>
       <RBSheet
@@ -81,123 +85,134 @@ function OrderStatus({ refRBSheet, isReceiver, navigation, id }) {
           container: styles.container,
         }}
       >
-        <View style={styles.view}>
-          <View style={styles.imageContainer}>
-            <Image
-              resizeMode="stretch"
-              source={
-                load
-                  ? { uri: main.baseLink + result.data.data.order_info.image1 }
-                  : ""
-              }
-              style={styles.img}
-            />
-            <Image
-              resizeMode="stretch"
-              source={
-                load
-                  ? { uri: main.baseLink + result.data.data.order_info.image2 }
-                  : ""
-              }
-              style={styles.img}
-            />
-          </View>
-          <View style={styles.orderStatus}>
-            {isReceiver && (
-              <LightInput
-                text={"Current\nstatus"}
-                editable={false}
-                contextMenuHidden={true}
-                value={value}
-                style={styles.lightInput}
-                textStyle={styles.text}
-              />
-            )}
-            {!isReceiver && (
-              <>
-                <View style={styles.statusContainer}>
-                  <Text style={styles.statusText}>{"Current\nstatus"}</Text>
-                  <DropDownPicker
-                    placeholder="Status"
-                    open={open}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <View style={styles.view}>
+              <View style={styles.imageContainer}>
+                <Image
+                  resizeMode="stretch"
+                  source={
+                    load
+                      ? {
+                          uri:
+                            main.baseLink + result.data.data.order_info.image1,
+                        }
+                      : ""
+                  }
+                  style={styles.img}
+                />
+                <Image
+                  resizeMode="stretch"
+                  source={
+                    load
+                      ? {
+                          uri:
+                            main.baseLink + result.data.data.order_info.image2,
+                        }
+                      : ""
+                  }
+                  style={styles.img}
+                />
+              </View>
+              <View style={styles.orderStatus}>
+                {isReceiver && (
+                  <LightInput
+                    text={"Current\nstatus"}
+                    editable={false}
+                    contextMenuHidden={true}
                     value={value}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setValue}
-                    setItems={setItems}
-                    disabled={isReceiver}
-                    showArrowIcon={!isReceiver}
-                    textStyle={styles.textStyle}
-                    containerStyle={styles.containerStyle}
-                    style={styles.style}
-                    dropDownContainerStyle={styles.dropDownContainerStyle}
-                    ArrowUpIconComponent={() => (
-                      <AntDesign name="up" size={24} color="white" />
-                    )}
-                    ArrowDownIconComponent={() => (
-                      <AntDesign name="down" size={24} color="white" />
-                    )}
+                    style={styles.lightInput}
+                    textStyle={styles.text}
                   />
-                </View>
-              </>
-            )}
+                )}
+                {!isReceiver && (
+                  <>
+                    <View style={styles.statusContainer}>
+                      <Text style={styles.statusText}>{"Current\nstatus"}</Text>
+                      <DropDownPicker
+                        placeholder="Status"
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                        disabled={isReceiver}
+                        showArrowIcon={!isReceiver}
+                        textStyle={styles.textStyle}
+                        containerStyle={styles.containerStyle}
+                        style={styles.style}
+                        dropDownContainerStyle={styles.dropDownContainerStyle}
+                        ArrowUpIconComponent={() => (
+                          <AntDesign name="up" size={24} color="white" />
+                        )}
+                        ArrowDownIconComponent={() => (
+                          <AntDesign name="down" size={24} color="white" />
+                        )}
+                      />
+                    </View>
+                  </>
+                )}
 
-            <TouchableOpacity
-              style={styles.time}
-              onPress={() => setopenTime(isReceiver ? false : true)}
-            >
-              <LightInput
-                text={"Average\ntime"}
-                editable={false}
-                contextMenuHidden={true}
-                value={`${date.getHours()}:${date.getMinutes()}`}
-                style={styles.lightInput}
-                textStyle={styles.text}
-              />
-            </TouchableOpacity>
-            <LightInput
-              text={"Current\nlocation"}
-              editable={!isReceiver}
-              contextMenuHidden={isReceiver}
-              value={location}
-              setValue={setLocation}
-              style={styles.lightInput}
-              textStyle={styles.text}
-            />
-          </View>
-          {!isReceiver && (
-            <AppButton
-              value={"Save"}
-              style={[styles.save, { backgroundColor: "grey" }]}
-              onPress={() => {}}
-            />
-          )}
-          <View style={styles.btnContainer}>
-            <SmallButton
-              value={"Received"}
-              color={colors.secondary}
-              onPress={() => {
-                refRBSheet.current.close();
-                receiveOrderBSheet.current.open();
-              }}
-            />
-            <SmallButton
-              value={`${isReceiver ? "Request" : "Live"}\nlocation`}
-              onPress={() => {
-                refRBSheet.current.close();
-                navigation.navigate("Location");
-              }}
-            />
-            <SmallButton
-              value={"Cancel"}
-              onPress={() => {
-                refRBSheet.current.close();
-                cancelOrderBSheet.current.open();
-              }}
-            />
-          </View>
-        </View>
-
+                <TouchableOpacity
+                  style={styles.time}
+                  onPress={() => setopenTime(isReceiver ? false : true)}
+                >
+                  <LightInput
+                    text={"Average\ntime"}
+                    editable={false}
+                    contextMenuHidden={true}
+                    value={`${date.getHours()}:${date.getMinutes()}`}
+                    style={styles.lightInput}
+                    textStyle={styles.text}
+                  />
+                </TouchableOpacity>
+                <LightInput
+                  text={"Current\nlocation"}
+                  editable={!isReceiver}
+                  contextMenuHidden={isReceiver}
+                  value={location}
+                  setValue={setLocation}
+                  style={styles.lightInput}
+                  textStyle={styles.text}
+                />
+              </View>
+              {!isReceiver && (
+                <AppButton
+                  value={"Save"}
+                  style={[styles.save, { backgroundColor: "grey" }]}
+                  onPress={() => {}}
+                />
+              )}
+              <View style={styles.btnContainer}>
+                <SmallButton
+                  value={"Received"}
+                  color={colors.secondary}
+                  onPress={() => {
+                    refRBSheet.current.close();
+                    receiveOrderBSheet.current.open();
+                  }}
+                />
+                <SmallButton
+                  value={`${isReceiver ? "Request" : "Live"}\nlocation`}
+                  onPress={() => {
+                    refRBSheet.current.close();
+                    navigation.navigate("Location");
+                  }}
+                />
+                <SmallButton
+                  value={"Cancel"}
+                  onPress={() => {
+                    refRBSheet.current.close();
+                    cancelOrderBSheet.current.open();
+                  }}
+                />
+              </View>
+            </View>
+          </>
+        )}
         {openTime && (
           <DateTimePicker
             value={date}
@@ -209,8 +224,15 @@ function OrderStatus({ refRBSheet, isReceiver, navigation, id }) {
           />
         )}
       </RBSheet>
-      <CancelOrder refRBSheet={cancelOrderBSheet} />
-      <ReceiveOrder refRBSheet={receiveOrderBSheet} />
+      <CancelOrder
+        refRBSheet={cancelOrderBSheet}
+        setRefreshing={setRefreshing}
+        pickupId={pickupId}
+      />
+      <ReceiveOrder
+        refRBSheet={receiveOrderBSheet}
+        setRefreshing={setRefreshing}
+      />
     </>
   );
 }
