3025c36ec27a890686f9306c1666b838b6bc6818
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-13 18:43:05 +0200

    Linking: Creating Loading state to be used in OrderInfo when request to chat

--- onYourWay/app/screens/MyOrderScreen/MyOrder.js
+++ onYourWay/app/screens/MyOrderScreen/MyOrder.js
@@ -14,6 +14,7 @@ import main from "../../config/main";
 function MyOrder({ navigation }) {
   const [loadData, setLoadData] = useState(false);
   const [refreshing, setRefreshing] = useState(true);
+  const [load, setLoad] = useState(false);
   const {
     isLoading,
     data: result,
@@ -34,7 +35,7 @@ function MyOrder({ navigation }) {
     setRefreshing(false);
   }, [result]);
 
-  if (isLoading) {
+  if (isLoading || load) {
     return <Loading />;
   }
   if (isError || (result && (result === 401 || result === 400))) {
@@ -85,6 +86,7 @@ function MyOrder({ navigation }) {
               pickerPhone={item.picked ? picker.phone : ""}
               pickerAddress={item.picked ? picker.address : ""}
               pickerRate={item.picked ? picker.rate : ""}
+              setIsLoading={setLoad}
             />
           );
         }}
