16fb5025ad71ce09ef1d08a698ab16079a51d68c
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-13 23:22:02 +0200

    Linking: Adding the mutate function that will call cancel Order request when the cancel button is clicked

--- onYourWay/app/components/CancelOrder/CancelOrder.js
+++ onYourWay/app/components/CancelOrder/CancelOrder.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dimensions, Text, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
+import Toast from "react-native-root-toast";
+import { useMutation } from "react-query";
+import Loading from "../Loading/Loading";
 
 import Input from "../Input/Input.js";
 import AppButton from "../AppButton/AppButton";
 import styles from "./styles";
-function CancelOrder({ refRBSheet }) {
+import main from "../../config/main.js";
+
+function CancelOrder({ refRBSheet, setRefreshing, pickupId }) {
   const [reason, setReason] = useState("");
   const windowHeight = Dimensions.get("window").height;
+  const {
+    mutate,
+    isError,
+    isLoading,
+    error,
+    data: result,
+  } = useMutation(main.cancelOrder);
 
+  const onCancel = () => {};
   return (
     <RBSheet
       ref={refRBSheet}
@@ -21,28 +34,29 @@ function CancelOrder({ refRBSheet }) {
         container: styles.container,
       }}
     >
-      <View style={styles.view}>
-        <Text style={styles.textTitle}>Are you sure ?</Text>
-        <Text style={styles.textSmall}>
-          Please specify why You are cancelling
-        </Text>
-        <Input
-          text="Reason"
-          value={reason}
-          setValue={setReason}
-          multiline={true}
-          style={styles.input}
-        />
-        <Text style={styles.note}>
-          Note: that will affect your rate if the pickup accepted
-        </Text>
-        <AppButton
-          value={"Cancel"}
-          onPress={() => {
-            refRBSheet.current.close();
-          }}
-        />
-      </View>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <View style={styles.view}>
+            <Text style={styles.textTitle}>Are you sure ?</Text>
+            <Text style={styles.textSmall}>
+              Please specify why You are cancelling
+            </Text>
+            <Input
+              text="Reason"
+              value={reason}
+              setValue={setReason}
+              multiline={true}
+              style={styles.input}
+            />
+            <Text style={styles.note}>
+              Note: that will affect your rate if the pickup accepted
+            </Text>
+            <AppButton value={"Cancel"} onPress={onCancel} />
+          </View>
+        </>
+      )}
     </RBSheet>
   );
 }
