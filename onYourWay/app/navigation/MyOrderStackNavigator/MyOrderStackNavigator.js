ac0457a69471e1542ca11201954147be4c8fd738
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-13 18:34:03 +0200

    Linking:Adding In Chat screen to My Order Stack Navigator to allow navigate to it form my order screen

--- onYourWay/app/navigation/MyOrderStackNavigator/MyOrderStackNavigator.js
+++ onYourWay/app/navigation/MyOrderStackNavigator/MyOrderStackNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import MyOrder from "../../screens/MyOrderScreen/MyOrder";
 import AddOrder from "../../screens/AddOrderScreen/AddOrder";
 import Location from "../../screens/LocationScreen/Location";
+import InChat from "../../screens/InChatScreen/InChat";
 
 function MyOrderStackNavigator() {
   const Stack = createStackNavigator();
@@ -22,6 +23,11 @@ function MyOrderStackNavigator() {
       <Stack.Screen
         name="Location"
         component={Location}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="InChat"
+        component={InChat}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
