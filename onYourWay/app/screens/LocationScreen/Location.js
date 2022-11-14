9f79e2fc61e8ead63078f6619ee841caca3b5f47
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-15 00:28:04 +0200

    Linking: Removing testing data and creating a new state for the picker user location

--- onYourWay/app/screens/LocationScreen/Location.js
+++ onYourWay/app/screens/LocationScreen/Location.js
@@ -2,33 +2,39 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
 import MapView from "react-native-maps";
 import { Marker, Polygon } from "react-native-maps";
-import * as geoLocation from "expo-location"; 
+import * as geoLocation from "expo-location";
 import MapViewDirections from "react-native-maps-directions";
+import Toast from "react-native-root-toast";
 
 import styles from "./styles";
 import Navbar from "../../components/Navbar/Navbar";
 import colors from "../../config/colors";
 import Constants from "expo-constants";
 import Loading from "../../components/Loading/Loading";
+import { useQuery } from "react-query";
+import main from "../../config/main";
 
-function Location({ navigation }) {
+function Location({ navigation, route }) {
+  const { pickupId } = route.params;
   const [load, setLoad] = useState(false);
   const [myLocation, setMyLocation] = useState({
     latitude: 33.450736,
     longitude: 35.396315,
   });
+  const [userLocation, setUserLocation] = useState({
+    latitude: 33.450736,
+    longitude: 35.396315,
+  }); 
 
   useEffect(() => {
-    setLoad(true);
     const getLocation = async () => {
+      setLoad(true);
       let { status } = await geoLocation.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         alert("Permission to access location was denied");
         return;
       }
-
       let getLocation = await geoLocation.getCurrentPositionAsync({});
-      //  console.log("my ", getLocation);
       setMyLocation({
         latitude: getLocation.coords.latitude,
         longitude: getLocation.coords.longitude,
@@ -39,7 +45,7 @@ function Location({ navigation }) {
   }, []);
 
   if (load) {
-    return <Loading/>
+    return <Loading />;
   }
 
   return (
@@ -54,33 +60,19 @@ function Location({ navigation }) {
           longitudeDelta: 0.0421,
         }}
         showsUserLocation={true}
-        onUserLocationChange={(e) => {
-          //   console.log("e", e.nativeEvent.coordinate);
-          // setMyLocation({
-          //   longitude: e.nativeEvent.longitude,
-          //   latitude: e.nativeEvent.latitude,
-          // });
-        }}
       >
         <Marker coordinate={myLocation} title={"Your Location"} />
 
         <MapViewDirections
-          origin={{
-            latitude: 33.88863,
-            longitude: 35.49548,
-          }}
+          origin={userLocation}
           destination={myLocation}
           apikey={Constants.manifest.extra.googleApiKey}
           strokeWidth={3}
           strokeColor={colors.darker}
         />
-        <Marker
-          coordinate={{
-            latitude: 33.88863,
-            longitude: 35.49548,
-          }}
-          title={"Picker Location"}
-        />
+
+        <Marker coordinate={userLocation} title={"Picker Location"} />
+
       </MapView>
     </SafeAreaView>
   );
