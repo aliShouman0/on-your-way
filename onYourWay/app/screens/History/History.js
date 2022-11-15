42151cb8657678f4da7eeec93eadf9cf23faa4c0
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-15 21:33:17 +0200

    Linking: Removing all testing data and adding the request use query with all necessary imports

--- onYourWay/app/screens/History/History.js
+++ onYourWay/app/screens/History/History.js
@@ -1,78 +1,46 @@
-import React, { useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
-import DropDownCity from "../../components/DropDownCity/DropDownCity";
-import Navbar from "../../components/Navbar/Navbar";
-import OrderInfo from "../../components/OrderInfo/OrderInfo"; 
-import styles from "./styles";
+import { useQuery } from "react-query";
+import Toast from "react-native-root-toast";
 
-const testing = [
-  {
-    id: "id1",
-    userName: "Ali Alrida Shouman",
-    userImg: require("../../assets/user1.jpg"),
-    from: "Beirut",
-    to: "Byblos",
-    pay: "145000L.L",
-    orderImg: require("../../assets/keyboard.jpg"),
-    orderDescription: "keyboard and mouse",
-  },
-  {
-    id: "id2",
-    userName: "Baker",
-    userImg: require("../../assets/user1.jpg"),
-    from: "Saida",
-    to: "Nabatieh",
-    pay: "46000L.L",
-    orderImg: require("../../assets/keyboard.jpg"),
-    orderDescription: "Laptop",
-  },
-  {
-    id: "id3",
-    userName: "Aya Matok",
-    userImg: require("../../assets/user1.jpg"),
-    from: "Beirut",
-    to: "Tripoli",
-    pay: "745000L.L",
-    orderImg: require("../../assets/keyboard.jpg"),
-    orderDescription: "Sandwich",
-  },
-];
+import Loading from "../../components/Loading/Loading";
+import Navbar from "../../components/Navbar/Navbar";
+import OrderInfo from "../../components/OrderInfo/OrderInfo";
+import styles from "./styles";
+import main from "../../config/main";
 
 function History({ navigation }) {
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
-  const [refreshing, setRefreshing] = useState(false);
-  const [data, setData] = useState(testing);
+  const [loadData, setLoadData] = useState(false);
+  const [refreshing, setRefreshing] = useState(true);
+  const [load, setLoad] = useState(false);
+  const isFocused = useIsFocused();
+  const {
+    isLoading,
+    data: result,
+    isError,
+    error,
+    refetch,
+  } = useQuery("myCompleteOrder", main.getEndedOrder, {
+    refetchOnMount: "always",
+    retryOnMount: true,
+    enabled: false,
+  });
 
-
-  const refresh = () => {
-    setData([
-      {
-        id: "id3",
-        userName: "Refresh Done",
-        userImg: require("../../assets/user1.jpg"),
-        from: "Beirut",
-        to: "Tripoli",
-        pay: "745000L.L",
-        orderImg: require("../../assets/keyboard.jpg"),
-        orderDescription: "Refresh Done",
-      },
-    ]);
-  };
+ 
 
   return (
     <SafeAreaView style={styles.mainView}>
       <Navbar type={"main"} title={"History"} navigation={navigation} />
-      <View style={styles.container}>
-        <DropDownCity placeholder={"From"} setValue={setFrom} value={from} />
-        <DropDownCity placeholder={"To"} setValue={setTo} value={to} />
-      </View>
       <FlatList
         style={styles.flatList}
         keyExtractor={(data) => data.id.toString()}
-        data={data}
+        data={loadData ? result.data.data : []}
         refreshing={refreshing}
-        onRefresh={refresh}
+        onRefresh={() => {
+          setLoadData(false);
+          refetch();
+        }}
         renderItem={({ item, index, separators }) => (
           <OrderInfo
             key={item.id}
