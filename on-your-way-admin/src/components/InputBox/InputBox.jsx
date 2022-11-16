962a0d862f7677b24c2b2e3b03fca85557f4ea10
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 16:30:58 +0200

    Frontend: Creating Custom input box component

---
+++ on-your-way-admin/src/components/InputBox/InputBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputBox({ value, setValue, type, htmlFor, placeholder }) {
+  return <div className="flex flex-col w-full my-3"></div>;
+}
+
+export default InputBox;
