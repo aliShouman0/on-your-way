b0d192af5d20871082c25e3d7cf69dde97edbdd6
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 16:38:07 +0200

    Frontend: Adding BrowserRouter and define first route as Login page

--- on-your-way-admin/src/App.js
+++ on-your-way-admin/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
