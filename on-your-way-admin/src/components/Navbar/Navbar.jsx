c8809e38f8c249c3ac067c4199d9716656e33522
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 22:09:45 +0200

    Frontend: Adding user name and image section with the styling

--- on-your-way-admin/src/components/Navbar/Navbar.jsx
+++ on-your-way-admin/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import user from "../../assets/user.png";
 
-function Navbar() { 
-  return <div></div>;
+function Navbar() {
+  const userName = "Ali Alrida";
+  return (
+    <div className="flex fixed top-0 right-0 items-center justify-end w-full bg-primary p-3 z-10">
+      <span className="mx-5 text-white text-base">{userName}</span>
+      <div className="w-12 h-1/4 mr-1">
+        <img src={user} alt="logo" />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
