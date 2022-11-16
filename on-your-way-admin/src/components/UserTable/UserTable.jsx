3c87f106d3f079abb23722f67da36ff56b245327
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 01:48:21 +0200

    Frontend: Creating body part by mapping on all data array that passed by props and return rows

--- on-your-way-admin/src/components/UserTable/UserTable.jsx
+++ on-your-way-admin/src/components/UserTable/UserTable.jsx
@@ -36,6 +36,42 @@ function UserTable({ onImageId, onVerified, data }) {
             </th>
           </tr>
         </thead>
+        <tbody>
+          {data.map((data, i) => (
+            <tr className="bg-transparent " key={data.id}>
+              <td className="py-4 px-4">
+                <div className="w-12 h-1/4  ">
+                  <img src={data.avatar} alt="logo" />
+                </div>
+              </td>
+              <td className="py-4 px-4">{data.name}</td>
+              <td className="py-4 px-4">{data.email}</td>
+              <td className="py-4 px-4">{data.phone}</td>
+              <td className="py-4 px-4">{data.address}</td>
+              <td className="py-4 px-4">{data.birthday}</td>
+              <td className="py-4 px-4">{data.rate}</td>
+              <td className="py-4 px-4">
+                <div
+                  className="w-12 h-1/4  "
+                  onClick={() => onImageId(data.id)}
+                >
+                  <img src={data.idImage} alt="logo" />
+                </div>
+              </td>
+              <td className="py-4 px-4">
+                <Switch
+                  id={data.id}
+                  onChange={(checked, event, id) => onVerified(checked, id)}
+                  checked={data.verified}
+                  onColor={colors.secondary}
+                  offColor={colors.primary}
+                  width={48}
+                  height={20}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
