c25a898999024f4df9d435463ce2c419f3b5350b
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 23:50:00 +0200

    Frontend: Adding  routes for users page

--- on-your-way-admin/src/App.js
+++ on-your-way-admin/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Users from "./pages/Users/Users";
+import Orders from "./pages/Orders/Orders";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path={"/"} element={<Login />} />
         <Route path={"/login"} element={<Login />} />
         <Route path={"/dashboard"} element={<Dashboard />} />
+        <Route path={"/users"} element={<Users />} /> 
+
+
       </Routes>
     </BrowserRouter>
   );
