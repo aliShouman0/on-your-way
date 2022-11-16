ccecb4363c449cc77cf653a358bbcda5b971e991
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 16:34:43 +0200

    Frontend: Adding label and styles for Input Box components

--- on-your-way-admin/src/components/InputBox/InputBox.jsx
+++ on-your-way-admin/src/components/InputBox/InputBox.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from "react"; 
 
 function InputBox({ value, setValue, type, htmlFor, placeholder }) {
-  return <div className="flex flex-col w-full my-3"></div>;
+  return (
+    <div className="flex flex-col w-full my-3">
+      <label htmlFor={htmlFor} className="text-white text-lg font-normal mb-2">
+        {placeholder}
+      </label>
+      <input
+        type={type}
+        value={value}
+        id={htmlFor}
+        onChange={(e) => setValue(e.target.value)}
+        className={`p-3 w-full border-none  outline-none rounded-lg `}
+        placeholder={placeholder}
+      />
+    </div>
+  );
 }
 
 export default InputBox;
