e0ef58662c727c10ffbb17b697ea8b5bd95cd497
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 16:53:10 +0200

    Frontend: Adding main logo and define all needed states in login page

--- on-your-way-admin/src/pages/Login/Login.jsx
+++ on-your-way-admin/src/pages/Login/Login.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-import colors from "../../constants/colors";
 import logo from "../../assets/logo-gold.png";
+import { useNavigate } from "react-router-dom";
+import InputBox from "../../components/InputBox/InputBox";
+import Loading from "../../components/Loading/Loading";
 
 function Login() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
+  const [error, setError] = useState(false);
+  const [load, setLoad] = useState(false);
+
+  if (load) {
+    return <Loading />;
+  }
+
+  return (
+    <div
+      className={`bg-primary h-screen w-screen overflow-hidden flex flex-col items-center justify-center`}
+    >
+      <div className="w-1/6 h-1/6 mb-16">
+        <img src={logo} alt="Logo" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
