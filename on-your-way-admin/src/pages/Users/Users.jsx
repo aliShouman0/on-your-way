35c04aeea68e7d61ae80d37b631e2c3f3a9db7af
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 01:50:47 +0200

    Frontend: Adding User Table components in user page with testing data

--- on-your-way-admin/src/pages/Users/Users.jsx
+++ on-your-way-admin/src/pages/Users/Users.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
+
+import user from "../../assets/user.png";
 import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Navbar from "../../components/Navbar/Navbar";
 import Search from "../../components/Search/Search";
+import UserTable from "../../components/UserTable/UserTable";
 
 function Users() {
   const [search, setSearch] = useState("");
+  const test = [
+    {
+      id: "1",
+      avatar: user,
+      name: "Alex",
+      email: "me@",
+      phone: "7891423",
+      address: "Saida",
+      birthday: "3/10/2000",
+      rate: "3",
+      idImage: user,
+      verified: true,
+    },
+  ];
   return (
     <div className=" w-full h-screen bg-dark   overflow-x-hidden ">
       <Navbar />
@@ -14,7 +31,17 @@ function Users() {
           <p className="text-white text-4xl  font-bold text-left">Users</p>
           <Search value={search} setValue={setSearch} placeholder="Search" />
         </div>
-        <div className="flex flex-col justify-center items-center"></div>
+        <div className="flex flex-col justify-center items-center mt-10">
+          <UserTable
+            data={test}
+            onVerified={(checked, id) => {
+              alert(id, checked);
+            }}
+            onImageId={(id) => {
+              alert(id);
+            }}
+          />
+        </div>
       </section>
     </div>
   );
