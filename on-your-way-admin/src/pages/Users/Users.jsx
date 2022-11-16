840e3c3370b2344bc5ab704f6117da7548df0e93
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 00:14:03 +0200

    Frontend: Adding the custom search component and create its useState

--- on-your-way-admin/src/pages/Users/Users.jsx
+++ on-your-way-admin/src/pages/Users/Users.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Navbar from "../../components/Navbar/Navbar";
+import Search from "../../components/Search/Search";
 
 function Users() {
+  const [search, setSearch] = useState("");
   return (
     <div className=" w-full h-screen bg-dark   overflow-x-hidden ">
       <Navbar />
       <LeftPanel active={"users"} />
       <section className="absolute top-[10%] right-0 h-auto w-3/4 p-5 flex flex-col bg-dark  ">
-        <p className="text-white text-4xl mt-5 font-bold text-left">Users</p>
+        <div className="flex justify-between mt-5 items-center">
+          <p className="text-white text-4xl  font-bold text-left">Users</p>
+          <Search value={search} setValue={setSearch} placeholder="Search" />
+        </div>
         <div className="flex flex-col justify-center items-center"></div>
       </section>
     </div>
