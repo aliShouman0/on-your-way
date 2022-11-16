18aa0bc9dde78df362223690b910ed3a6d11111e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 16:35:46 +0200

    Frontend: Adding custom color in tailwind

--- on-your-way-admin/tailwind.config.js
+++ on-your-way-admin/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#313940",
+        secondary: "#FFC700",
+        dark: "#4E5C68",
+        darker: "#1B2126",
+        black: "#000",
+        white: "#fff",
+        lightWhite: "rgba(255,255, 255, 0.5)'",
+      },
+    },
   },
   plugins: [],
-}
+};
