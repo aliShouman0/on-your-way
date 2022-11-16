ef235e4482fa073364bb521df9f0250bb7fd6725
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 02:08:52 +0200

    Linking: Creating a function related to the handling error result of adding a new Pickup

--- onYourWay/app/components/NewOrderInfo/controller.js
+++ onYourWay/app/components/NewOrderInfo/controller.js
@@ -1,3 +1,27 @@
 import { useEffect } from "react";
 import Toast from "react-native-root-toast";
 
+const addPickupResultUseEffect = (
+  setLoad,
+  pickupIsError,
+  pickupResult,
+  pickupError,
+  refetchStatus
+) => {
+  return useEffect(() => {
+    setLoad(false);
+    if (
+      pickupIsError ||
+      (pickupResult &&
+        (pickupResult === 401 ||
+          pickupResult === 400 ||
+          pickupResult === 0 ||
+          pickupResult === 500))
+    ) {
+      Toast.show("Some Thing went Wrong 😔", {
+        duration: Toast.durations.LONG,
+      });
+      console.log(pickupError);
+    }
+  }, [pickupResult, pickupIsError]);
+};
