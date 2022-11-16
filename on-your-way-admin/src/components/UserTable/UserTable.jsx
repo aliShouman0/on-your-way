db86612590593d2f724d0c6bff5f805ed705396d
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 01:42:56 +0200

    Frontend: Creating  Table and add title row thead with styling

--- on-your-way-admin/src/components/UserTable/UserTable.jsx
+++ on-your-way-admin/src/components/UserTable/UserTable.jsx
@@ -4,7 +4,41 @@ import Switch from "react-switch";
 import colors from "../../constants/colors";
 
 function UserTable({ onImageId, onVerified, data }) {
-  return <div></div>;
+  return (
+    <div className="overflow-x-auto relative">
+      <table className="w-full text-sm text-center text-white  bg-transparent">
+        <thead className="text-sm text-white  uppercase bg-transparent ">
+          <tr>
+            <th scope="col" className="py-3 px-4"></th>
+            <th scope="col" className="py-3 px-4">
+              Name
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Email
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Phone
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Address
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Birthday
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Rate
+            </th>
+            <th scope="col" className="py-3 px-4">
+              ID
+            </th>
+            <th scope="col" className="py-3 px-4">
+              Verified
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default UserTable;
