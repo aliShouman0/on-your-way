82cf03bdb3d251b4b11d314a5257469c4aa5d1aa
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 23:44:42 +0200

    Frontend: Creating user page

---
+++ on-your-way-admin/src/pages/Users/Users.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import LeftPanel from "../../components/LeftPanel/LeftPanel";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Users() {
+  return <div></div>;
+}
+
+export default Users;
