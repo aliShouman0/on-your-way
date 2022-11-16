17b9c83a432684327ce5ae2378e9b64b2da317b3
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 23:48:47 +0200

    Frontend: Adding Navbar ,Left Panel component and main section with styling

--- on-your-way-admin/src/pages/Users/Users.jsx
+++ on-your-way-admin/src/pages/Users/Users.jsx
@@ -3,7 +3,16 @@ import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Navbar from "../../components/Navbar/Navbar";
 
 function Users() {
-  return <div></div>;
+  return (
+    <div className=" w-full h-screen bg-dark   overflow-x-hidden ">
+      <Navbar />
+      <LeftPanel active={"users"} />
+      <section className="absolute top-[10%] right-0 h-auto w-3/4 p-5 flex flex-col bg-dark  ">
+        <p className="text-white text-4xl mt-5 font-bold text-left">Users</p>
+        <div className="flex flex-col justify-center items-center"></div>
+      </section>
+    </div>
+  );
 }
 
 export default Users;
