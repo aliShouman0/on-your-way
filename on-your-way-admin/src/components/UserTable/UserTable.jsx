aa19c582386c5957c5dd618d99980d8673342e6b
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 01:39:13 +0200

    Frontend: Creating user Table custom  Components

---
+++ on-your-way-admin/src/components/UserTable/UserTable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Switch from "react-switch";
+
+import colors from "../../constants/colors";
+
+function UserTable({ onImageId, onVerified, data }) {
+  return <div></div>;
+}
+
+export default UserTable;
