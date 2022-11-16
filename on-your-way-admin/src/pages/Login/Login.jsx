cd50ad5c0469d85c773f8875fd579690c5bacbab
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 14:37:04 +0200

    Frontend: Creating and initializing the Login page

---
+++ on-your-way-admin/src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import colors from "../../constants/colors";
+import logo from "../../assets/logo-gold.png";
+
+function Login() {
+  return <div></div>;
+}
+
+export default Login;
