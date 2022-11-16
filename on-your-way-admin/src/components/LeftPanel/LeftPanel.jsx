e17587ef5376081250ac69dca16f4712c4e93246
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 22:00:10 +0200

    Frontend: Creating Left Panel component

---
+++ on-your-way-admin/src/components/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { MdDashboard } from "react-icons/md";
+import { HiUsers } from "react-icons/hi";
+import { GrDropbox } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
+
+import logo from "../../assets/logo-gold.png";
+
+function LeftPanel({ active }) {
+  const navigate = useNavigate();
+  return (
+    <aside className="">
+      {" "}
+    </aside>
+  );
+}
+
+export default LeftPanel;
