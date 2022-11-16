38fad4c6abb2c51812d724591a7cdc7b1b3117b7
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-16 17:04:08 +0200

    Frontend: Adding dashboard in routes adding its path

--- on-your-way-admin/src/App.js
+++ on-your-way-admin/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Login />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/dashboard"} element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
