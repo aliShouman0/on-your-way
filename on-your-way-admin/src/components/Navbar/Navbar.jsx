fb6fe33478aebc171161d5ba011c4af9430de5b3
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-18 00:07:51 +0200

    Linking: Adding/show current user information in navbar

--- on-your-way-admin/src/components/Navbar/Navbar.jsx
+++ on-your-way-admin/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import user from "../../assets/user.png";
+import { BASE_STORAGE } from "../../constants/constants";
 
-function Navbar() {
-  const userName = "Ali Alrida";
+function Navbar({ error }) {
+  const { name, avatar } = JSON.parse(localStorage.getItem("user_info"));
   return (
-    <div className="flex fixed top-0 right-0 items-center justify-end w-full bg-primary p-3 z-10">
-      <span className="mx-5 text-white text-base">{userName}</span>
-      <div className="w-12 h-1/4 mr-1">
-        <img src={user} alt="logo" />
+    <>
+      {error && (
+        <p className="fixed left-1/4 ml-3 top-3 z-[1000] text-lg font-bold text-secondary animate-pulse">
+          *Some Thing Went Wrong
+        </p>
+      )}
+      <div className="flex fixed top-0 right-0 items-center justify-end w-full bg-primary p-3 z-10">
+        <span className="mx-5 text-white text-base">{name}</span>
+        <div className="w-12 h-1/4 mr-1">
+          <img
+            src={`${BASE_STORAGE}/${avatar}`}
+            alt="logo"
+            className="rounded-full"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
