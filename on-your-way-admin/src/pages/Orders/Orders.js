045a08ca11d20cc290a4b7266d8369e28c8a370e
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 13:49:11 +0200

    Frontend: Adding search component and creating a states to use to open the popups

--- on-your-way-admin/src/pages/Orders/Orders.js
+++ on-your-way-admin/src/pages/Orders/Orders.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
+import user from "../../assets/user.png";
+import key from "../../assets/keyboard .jpg";
 import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Navbar from "../../components/Navbar/Navbar";
+import OrderInfo from "../../components/OrderInfo/OrderInfo";
+import PikerInfo from "../../components/PikerInfo/PikerInfo";
+import OrderComments from "../../components/OrderComments/OrderComments";
+import Search from "../../components/Search/Search";
 
 function Orders() {
+  const [pikerInfo, setPikerInfo] = useState(0);
+  const [orderComments, setOrderComments] = useState(0);
+  const [search, setSearch] = useState("");
   return (
     <div className=" w-full h-screen bg-dark   overflow-x-hidden ">
       <Navbar />
       <LeftPanel active={"orders"} />
       <section className="absolute top-[10%] right-0 h-auto w-3/4 p-5 flex flex-col bg-dark  ">
-        <p className="text-white text-4xl mt-5 font-bold text-left">Orders</p>
-        <div className="flex flex-col justify-center items-center"> </div>
+        <div className="flex justify-between mt-5 items-center">
+          <p className="text-white text-4xl  font-bold text-left">Orders</p>
+          <Search value={search} setValue={setSearch} placeholder="Search" />
+        </div>
+        <div className="flex flex-wrap justify-center items-center  mt-10"></div>
       </section>
     </div>
   );
