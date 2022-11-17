2116bbca0167293663c74899e545aa909470e8cd
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 23:58:09 +0200

    Linking: Adding options for bar chart

--- on-your-way-admin/src/pages/Dashboard/controller.js
+++ on-your-way-admin/src/pages/Dashboard/controller.js
@@ -5,3 +5,30 @@ export const extractor = (data, key) => {
     return element[key];
   });
 };
+
+export const options = {
+  responsive: true,
+  legend: {
+    labels: {
+      fontColor: "white",
+      color: "black",
+      fontSize: 120,
+    },
+  },
+  scales: {
+    yAxes: {
+      ticks: {
+        beginAtZero: true,
+        color: "black",
+        fontSize: 12,
+      },
+    },
+    xAxes: {
+      ticks: {
+        beginAtZero: true,
+        color: "black",
+        fontSize: 12,
+      },
+    },
+  },
+};
