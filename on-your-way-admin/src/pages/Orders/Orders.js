f1eb0a7cce146afad9c0e7964240c40a7fc60b9d
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 02:12:33 +0200

    frontend:Adding Left Panel and Navbar and creating main section with styling

--- on-your-way-admin/src/pages/Orders/Orders.js
+++ on-your-way-admin/src/pages/Orders/Orders.js
@@ -4,7 +4,16 @@ import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Navbar from "../../components/Navbar/Navbar";
 
 function Orders() {
-  return <div></div>;
+  return (
+    <div className=" w-full h-screen bg-dark   overflow-x-hidden ">
+      <Navbar />
+      <LeftPanel active={"orders"} />
+      <section className="absolute top-[10%] right-0 h-auto w-3/4 p-5 flex flex-col bg-dark  ">
+        <p className="text-white text-4xl mt-5 font-bold text-left">Orders</p>
+        <div className="flex flex-col justify-center items-center"> </div>
+      </section>
+    </div>
+  );
 }
 
 export default Orders;
