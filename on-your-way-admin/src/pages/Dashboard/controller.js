473279e5049e02bd086b35c5671185fec4e71f91
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-18 00:02:23 +0200

    Linking: Creating load functions that get data from the response /result then filter/map it to get the needed structure then pass it to the bar chart

--- on-your-way-admin/src/pages/Dashboard/controller.js
+++ on-your-way-admin/src/pages/Dashboard/controller.js
@@ -32,3 +32,47 @@ export const options = {
     },
   },
 };
+
+export const loadRate = (data) => {
+  const usersRateNames = extractor(data, "name");
+  const rate = extractor(data, "rate");
+  const orderCount = extractor(data, "order_count");
+  const rates = rate.map((el, i) => el / orderCount[i]);
+  const userRateData = {
+    labels: usersRateNames,
+    datasets: [
+      {
+        label: "User/Rate",
+        data: rates,
+        backgroundColor: colors.darker,
+      },
+    ],
+  };
+  const userOrderCountData = {
+    labels: usersRateNames,
+    datasets: [
+      {
+        label: "User/Order Count",
+        data: orderCount,
+        backgroundColor: colors.white,
+      },
+    ],
+  };
+  return { userOrderCountData, userRateData };
+};
+
+export const loadProfit = (data) => {
+  const usersProfitNames = extractor(data, "name");
+  const profit = extractor(data, "profit");
+  const dataProfit = {
+    labels: usersProfitNames,
+    datasets: [
+      {
+        label: "User/Profits",
+        data: profit,
+        backgroundColor: colors.secondary,
+      },
+    ],
+  };
+  return dataProfit;
+};
