f67749e289e81a928d4e2ac884aa6d4ff1011e85
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 13:29:08 +0200

    Frontend: Creating Piker Info component that will be as a popup to show the picker information

---
+++ on-your-way-admin/src/components/PikerInfo/PikerInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import InputBox from "../InputBox/InputBox";
+import Rate from "../Rate/Rate";
+
+function PikerInfo({ image, email, name, phone, address, rate, close }) {
+  return <div></div>;
+}
+
+export default PikerInfo;
