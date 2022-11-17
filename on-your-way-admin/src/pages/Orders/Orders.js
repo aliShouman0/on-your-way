878b3159f632a96c1c28c18468218a7eb07ea381
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 02:09:47 +0200

    frontend:Creating orders screen

---
+++ on-your-way-admin/src/pages/Orders/Orders.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import LeftPanel from "../../components/LeftPanel/LeftPanel";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Orders() {
+  return <div></div>;
+}
+
+export default Orders;
