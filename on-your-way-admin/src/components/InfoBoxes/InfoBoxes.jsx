f69a67c3449b9237446dcb331bb96deb3f1f00f8
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 12:35:24 +0200

    Frontend: Creating the main section and add the 3 light infoboxes with styling

--- on-your-way-admin/src/components/InfoBoxes/InfoBoxes.jsx
+++ on-your-way-admin/src/components/InfoBoxes/InfoBoxes.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function InfoBoxes({ from, to, pay }) {
-  return <div></div>;
+  return (
+    <div className="flex w-full justify-between ">
+      <div className="border border-x-zinc-50 p-1 rounded-lg w-24   ">
+        <p className="text-xs font-extralight text-zinc-50">From</p>
+        <p className="font-semibold">{from}</p>
+      </div>
+      <div className="border border-x-zinc-50 p-1 rounded-lg w-24 ml-2">
+        <p className="text-xs font-extralight text-zinc-50">To</p>
+        <p className="font-semibold">{to}</p>
+      </div>
+      <div className="border border-x-zinc-50 p-1 rounded-lg w-24 ml-2 ">
+        <p className="text-xs font-extralight text-zinc-50">Pay</p>
+        <p className="font-semibold">{pay}</p>
+      </div>
+    </div>
+  );
 }
 
 export default InfoBoxes;
