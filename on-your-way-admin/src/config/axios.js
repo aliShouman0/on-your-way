13886534f7560e42d46dc6bcb3d147ff9956f779
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 18:48:10 +0200

    Linking: Creating get API function to be used when sending a get request and creating post Login ,getMyInfo function

--- on-your-way-admin/src/config/axios.js
+++ on-your-way-admin/src/config/axios.js
@@ -15,3 +15,27 @@ export const postAPI = async (url, data) => {
     return error.response.status;
   }
 };
+
+export const getAPI = async (url) => {
+  try {
+    const result = await axios.post(BASE_URL + url,  {
+      headers: {
+        Authorization: "Bearer  " + localStorage.getItem("access_token"),
+      },
+    });
+    return result;
+  } catch (error) {
+    console.log("Error from POST API ", error);
+    console.log("error.response ", error.response.data);
+    return error.response.status;
+  }
+};
+
+
+export const postLogin = async (data) => {
+  return await postAPI("/login", data);
+};
+
+export const getMyInfo = async () => {
+  return await postAPI("/me");
+};
