88542281d0b44373f19ff37d5498734863e1f953
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 12:56:59 +0200

    Frontend: Adding all needed props and creating user info section

--- on-your-way-admin/src/components/OrderInfo/OrderInfo.jsx
+++ on-your-way-admin/src/components/OrderInfo/OrderInfo.jsx
@@ -3,8 +3,35 @@ import InfoBoxes from "../InfoBoxes/InfoBoxes";
 import PikerInfo from "../PikerInfo/PikerInfo";
 import SharpInfoBoxes from "../SharpInfoBoxes/SharpInfoBoxes";
 
-function OrderInfo() {
-  return <div></div>;
+function OrderInfo({
+  id,
+  userImage,
+  userName,
+  mainImage,
+  image1,
+  image2,
+  description,
+  from,
+  to,
+  pay,
+  status,
+  AverageTime,
+  currentLocation,
+  piker,
+  comments,
+}) {
+  return (
+    <div className="bg-darker p-5 flex flex-col rounded-md text-white w-30 m-5">
+      <div className="flex items-center mt-2 mb-4 ">
+        <div className="w-12 h-1/5  ">
+          <img src={userImage} alt="userImage" />
+        </div>
+        <span className="text-md ml-5">{userName}</span>
+      </div>
+      <InfoBoxes from={from} to={to} pay={pay} />
+     
+    </div>
+  );
 }
 
 export default OrderInfo;
