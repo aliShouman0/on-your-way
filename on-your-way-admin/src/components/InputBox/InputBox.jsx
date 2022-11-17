d585920f31ad72f37f081975aa84bd4b54e1fff1
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 12:23:46 +0200

    Frontend:Editing input box add disabled attribute and add its styles when disabled is true

--- on-your-way-admin/src/components/InputBox/InputBox.jsx
+++ on-your-way-admin/src/components/InputBox/InputBox.jsx
@@ -1,6 +1,14 @@
-import React from "react"; 
+import React from "react";
 
-function InputBox({ value, setValue, type, htmlFor, placeholder }) {
+function InputBox({
+  value,
+  setValue,
+  type,
+  htmlFor,
+  placeholder,
+  disabled,
+  textarea,
+}) {
   return (
     <div className="flex flex-col w-full my-3">
       <label htmlFor={htmlFor} className="text-white text-lg font-normal mb-2">
@@ -11,8 +19,11 @@ function InputBox({ value, setValue, type, htmlFor, placeholder }) {
         value={value}
         id={htmlFor}
         onChange={(e) => setValue(e.target.value)}
-        className={`p-3 w-full border-none  outline-none rounded-lg `}
+        className={`p-3 w-full border-none  outline-none rounded-lg disabled:bg-transparent disabled:border-white disabled:border disabled:border-solid disabled:rounded-none disabled:text-white ${
+          textarea && "h-32  "
+        }`}
         placeholder={placeholder}
+        disabled={disabled}
       />
     </div>
   );
