ee4f668bababc742651002c262c7c54bb7973869
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 12:52:34 +0200

    Frontend: Creating Order Info component that will used in orders screen

---
+++ on-your-way-admin/src/components/OrderInfo/OrderInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import InfoBoxes from "../InfoBoxes/InfoBoxes";
+import PikerInfo from "../PikerInfo/PikerInfo";
+import SharpInfoBoxes from "../SharpInfoBoxes/SharpInfoBoxes";
+
+function OrderInfo() {
+  return <div></div>;
+}
+
+export default OrderInfo;
