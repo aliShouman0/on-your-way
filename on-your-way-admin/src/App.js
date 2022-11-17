a8e62070d0bcbfeb3c5b1a96676330de032b2e22
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 18:40:03 +0200

    Linking: Adding/Applying react query config add QueryClient and QueryClientProvider

--- on-your-way-admin/src/App.js
+++ on-your-way-admin/src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Users from "./pages/Users/Users";
 import Orders from "./pages/Orders/Orders";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<Login />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/dashboard"} element={<Dashboard />} />
-        <Route path={"/users"} element={<Users />} />
-        <Route path={"/Orders"} element={<Orders />} />
-
-
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Login />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/dashboard"} element={<Dashboard />} />
+          <Route path={"/users"} element={<Users />} />
+          <Route path={"/Orders"} element={<Orders />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
