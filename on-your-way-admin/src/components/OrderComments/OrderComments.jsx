4e05be835315254bdae11dba197445ba04819fe6
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 13:38:06 +0200

    Frontend: Creating Order Comments component that will show as a popup the comments and rate of pickup

---
+++ on-your-way-admin/src/components/OrderComments/OrderComments.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import InputBox from "../InputBox/InputBox";
+import Rate from "../Rate/Rate";
+
+function OrderComments({
+  pikerImage,
+  PikerName,
+  pikerRate,
+  pikerComment,
+  ReceiverImage,
+  ReceiverName,
+  ReceiverRate,
+  ReceiverComment,
+  close,
+}) {
+  return <div></div>;
+}
+
+export default OrderComments;
