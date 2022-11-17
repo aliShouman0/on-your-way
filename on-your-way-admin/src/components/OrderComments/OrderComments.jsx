61d817a1b9f47d447fc274b4bffed8530a45de5b
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 13:41:37 +0200

    Frontend: Adding popup styles and sections and creating a picker section to show the picker rate and comment

--- on-your-way-admin/src/components/OrderComments/OrderComments.jsx
+++ on-your-way-admin/src/components/OrderComments/OrderComments.jsx
@@ -13,7 +13,39 @@ function OrderComments({
   ReceiverComment,
   close,
 }) {
-  return <div></div>;
+  return (
+    <div className="fixed top-0 left-0 bg-[rgba(0,0,0,0.8)] h-screen w-screen z-[1000]">
+      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-12 bg-primary h-auto w-auto  rounded-lg">
+        <button
+          onClick={close}
+          className="absolute top-3 left-3 text-lg text-white font-extrabold"
+        >
+          X
+        </button>
+        <div className="flex items-center  justify-evenly h-full w-full mx-5">
+          <div className="flex flex-col  items-center">
+            <div className="w-1/2 h-1/4 ">
+              <img src={pikerImage} alt="userImage" />
+            </div>
+            <InputBox
+              type="text"
+              value={PikerName}
+              placeholder="Full Name"
+              disabled={true}
+            />
+            <InputBox
+              type="text"
+              value={pikerComment}
+              placeholder="Comments"
+              disabled={true}
+              textarea={true}
+            />
+            <Rate rate={pikerRate} size={24} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OrderComments;
