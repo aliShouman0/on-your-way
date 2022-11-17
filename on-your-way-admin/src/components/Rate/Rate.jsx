71c2d04877ba9989ba23e11326ce2aa4a0a335ae
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-17 12:01:55 +0200

    Frontend: Creating rate component that will show as stars the rate for a user

---
+++ on-your-way-admin/src/components/Rate/Rate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ImStarFull } from "react-icons/im";
+import colors from "../../constants/colors";
+
+function Rate({ rate, size }) {
+  return <div className=" "></div>;
+}
+Rate.defaultProps = {
+  size: -1,
+};
+export default Rate;
