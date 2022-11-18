3182b472b7a4a4a115e4a3f83db9d11b37f516cb
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-18 17:20:37 +0200

    Linking: Creating all needed request functions post and get

--- on-your-way-admin/src/config/axios.js
+++ on-your-way-admin/src/config/axios.js
@@ -5,6 +5,10 @@ export const postAPI = async (url, data) => {
   try {
     const result = await axios.post(BASE_URL + url, data, {
       headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Credentials": true,
         Authorization: "Bearer  " + localStorage.getItem("access_token"),
       },
     });
@@ -20,6 +24,10 @@ export const getAPI = async (url) => {
   try {
     const result = await axios(BASE_URL + url, {
       headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Credentials": true,
         Authorization: "Bearer  " + localStorage.getItem("access_token"),
       },
     });
@@ -30,7 +38,6 @@ export const getAPI = async (url) => {
     return error.response.status;
   }
 };
-
 
 export const postLogin = async (data) => {
   return await postAPI("/login", data);
@@ -50,4 +57,24 @@ export const getUsersRate = async () => {
 
 export const getUsersProfit = async () => {
   return await getAPI("/get_users_profit");
+};
+
+export const setVerifiedUser = async (data) => {
+  return await postAPI("/set_verified_user", data);
+};
+
+export const searchUser = async (like) => {
+  return await getAPI(`/search_user/${like}`);
+};
+
+export const getAllOrder = async () => {
+  return await getAPI(`/get_all_order`);
+};
+
+export const searchOrder = async (like) => {
+  return await getAPI(`/search_orders/${like}`);
+}; 
+
+export const setApprovedOrder = async (data) => {
+  return await postAPI("/set_approved_order", data);
 };
