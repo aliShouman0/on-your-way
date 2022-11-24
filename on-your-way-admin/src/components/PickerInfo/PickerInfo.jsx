5d219f24a25ef0265a274011e7a4a574c4805562
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-24 10:20:52 +0200

    Frontend: Improving styles for the picker information component, editing image styles, and making styles vertical

--- on-your-way-admin/src/components/PickerInfo/PickerInfo.jsx
+++ on-your-way-admin/src/components/PickerInfo/PickerInfo.jsx
@@ -5,19 +5,19 @@ import Rate from "../Rate/Rate";
 function PickerInfo({ image, email, name, phone, address, rate, close }) {
   return (
     <div className="fixed top-0 left-0  bg-[rgba(0,0,0,0.8)] h-screen w-screen z-[1000]">
-      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-8 bg-primary  h-auto w-auto   rounded-lg">
+      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-5 bg-primary  h-auto w-1/2   rounded-lg">
         <button
           onClick={close}
           className="absolute top-3 left-3 text-lg text-white font-extrabold"
         >
           X
         </button>
-        <div className="flex items-center  justify-center h-full w-full">
+        <div className="flex flex-col  items-center  justify-center h-full w-full">
           <div className="w-1/2 h-1/4 flex flex-col items-center justify-center mx-2 ">
-            <img src={image} alt="userImage" className="mb-3 rounded-full" />
+            <img src={image} alt="userImage" className="mb-3 w-40 h-40 rounded-full" />
             <Rate rate={rate} size={24} />
           </div>
-          <div className="w-1/2 h-full flex flex-col  items-center justify-center mx-2">
+          <div className="w-3/4 h-full flex flex-col  items-center justify-center mx-2">
             <InputBox
               type="text"
               value={name}
@@ -34,7 +34,7 @@ function PickerInfo({ image, email, name, phone, address, rate, close }) {
               htmlFor="phone"
               type="text"
               value={phone}
-              placeholder="phone"
+              placeholder="Phone Number"
               disabled={true}
             />
             <InputBox
