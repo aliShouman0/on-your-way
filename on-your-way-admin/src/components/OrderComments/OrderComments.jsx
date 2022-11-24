977be9e2921eeabd45b3cb84783a23562f5da951
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-24 09:58:40 +0200

    Frontend: Fixing small errors and editing images styles

--- on-your-way-admin/src/components/OrderComments/OrderComments.jsx
+++ on-your-way-admin/src/components/OrderComments/OrderComments.jsx
@@ -15,22 +15,25 @@ function OrderComments({
 }) {
   return (
     <div className="fixed top-0 left-0 bg-[rgba(0,0,0,0.8)] h-screen w-screen z-[1000]">
-      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-12 bg-primary h-auto w-auto  rounded-lg">
+      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center p-7 bg-primary h-auto w-auto  rounded-lg ">
         <button
           onClick={close}
           className="absolute top-3 left-3 text-lg text-white font-extrabold"
         >
           X
         </button>
-        <div className="flex items-center  justify-evenly h-full w-full mx-5">
-          <div className="flex flex-col  items-center">
-            <div className="w-1/2 h-1/4 ">
-              <img src={pikerImage} alt="userImage" className="rounded-full"/>
-            </div>
+        <div className="flex items-center  justify-around h-full w-full mx-5">
+          <div className="flex flex-col mx-5 items-center">
+            <img
+              src={pikerImage}
+              alt="userImage"
+              className=" w-24 h-24 rounded-full "
+            />
+
             <InputBox
               type="text"
               value={PikerName}
-              placeholder="Full Name"
+              placeholder="Shipper"
               disabled={true}
             />
             <InputBox
@@ -42,14 +45,17 @@ function OrderComments({
             />
             <Rate rate={pikerRate} size={24} />
           </div>
-          <div className="flex flex-col  items-center mx-5">
-            <div className="w-1/2 h-1/4 ">
-              <img src={ReceiverImage} alt="userImage" className="rounded-full"/>
-            </div>
+          <div className="flex flex-col mx-5 items-center  ">
+            <img
+              src={ReceiverImage}
+              alt="userImage"
+              className="w-24 h-24 rounded-full "
+            />
+
             <InputBox
               type="text"
               value={ReceiverName}
-              placeholder="Full Name"
+              placeholder="Receiver"
               disabled={true}
             />
             <InputBox
