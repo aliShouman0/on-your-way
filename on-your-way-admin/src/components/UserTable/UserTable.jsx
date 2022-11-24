f2aa699aa9fdbba9ea3a9dc9fde0897749b069f1
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-24 02:31:03 +0200

    Frontend: Editing styling decrease padding, and impoveing some styes

--- on-your-way-admin/src/components/UserTable/UserTable.jsx
+++ on-your-way-admin/src/components/UserTable/UserTable.jsx
@@ -6,63 +6,63 @@ import colors from "../../constants/colors";
 
 function UserTable({ onImageId, onVerified, data }) {
   return (
-    <div className="overflow-x-auto relative">
-      <table className="w-full text-sm text-center text-white  bg-transparent">
-        <thead className="text-sm text-white  uppercase bg-transparent ">
+    <div className="overflow-x-auto relative w-full  ">
+      <table className="w-full text-sm text-center text-white  bg-transparent  ">
+        <thead className="text-sm text-white font-extrabold uppercase bg-transparent ">
           <tr>
-            <th scope="col" className="py-3 px-4"></th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3  "></th>
+            <th scope="col" className="py-3 px-3">
               Name
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3 px-3">
               Email
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3 px-3">
               Phone
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3 px-3">
               Address
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3 px-3">
               Rate
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3 px-3 ">
               ID
             </th>
-            <th scope="col" className="py-3 px-4">
+            <th scope="col" className="py-3  ">
               Verified
             </th>
           </tr>
         </thead>
         <tbody>
           {data.map((data, i) => (
-            <tr className="bg-transparent text-xs" key={data.id}>
-              <td className="py-4 px-4">
-                <div className="w-12 h-1/4  ">
+            <tr className="bg-transparent text-sm" key={data.id}>
+              <td className="py-4 ">
+                <div className="w-12 h-12  rounded-full">
                   <img
                     src={`${BASE_STORAGE}/${data.avatar}`}
-                    className="rounded-full"
+                    className="rounded-full w-12 h-12  "
                     alt="userImage"
                   />
                 </div>
               </td>
-              <td className="py-4 px-4">{data.name}</td>
-              <td className="py-4 px-4">{data.email}</td>
-              <td className="py-4 px-4">{data.phone}</td>
-              <td className="py-4 px-4">{data.address}</td>
-              <td className="py-4 px-4">{data.order_count!==0? (data.rate/data.order_count).toFixed(2):0}</td>
-              <td className="py-4 px-4">
+              <td className="py-4 px-3">{data.name}</td>
+              <td className="py-4 px-3">{data.email}</td>
+              <td className="py-4 px-3">{data.phone}</td>
+              <td className="py-4 px-3">{data.address}</td>
+              <td className="py-4 px-3">{data.order_count!==0? (data.rate/data.order_count).toFixed(2):0}</td>
+              <td className="py-4 px-3   ">
                 <div
-                  className="w-12 h-1/4  "
+                  className="w-12 h-12 "
                   onClick={() => onImageId(data.id)}
                 >
                   <img
                     src={`${BASE_STORAGE}/${data.front_id_photo}`}
-                    alt="userImage"
+                    alt="userImage" 
                   />
                 </div>
               </td>
-              <td className="py-4 px-4">
+              <td className="py-4 ">
                 <Switch
                   id={`${data.id}`}
                   onChange={(checked, event, id) => onVerified(checked, id)}
