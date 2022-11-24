8ed7c641495fe8a8623fb0b10997450ea330326f
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-24 02:28:02 +0200

    Frontend: Adding styles props to overide styles when needed

--- on-your-way-admin/src/components/InputBox/InputBox.jsx
+++ on-your-way-admin/src/components/InputBox/InputBox.jsx
@@ -8,10 +8,11 @@ function InputBox({
   placeholder,
   disabled,
   textarea,
+  styles,
 }) {
   return (
-    <div className="flex flex-col w-full my-3">
-      <label htmlFor={htmlFor} className="text-white text-lg font-normal mb-2">
+    <div className="flex flex-col w-full my-3 ">
+      <label htmlFor={htmlFor} className="text-white text-xs font-bold   mb-2">
         {placeholder}
       </label>
       <input
@@ -19,7 +20,7 @@ function InputBox({
         value={value}
         id={htmlFor}
         onChange={(e) => setValue(e.target.value)}
-        className={`p-3 w-full border-none  outline-none rounded-lg disabled:bg-transparent disabled:border-white disabled:border disabled:border-solid disabled:rounded-none disabled:text-white ${
+        className={`p-3 w-full border-none  outline-none rounded-lg disabled:bg-transparent disabled:border-white disabled:border disabled:border-solid   disabled:text-white ${styles} ${
           textarea && "h-32  "
         }`}
         placeholder={placeholder}
