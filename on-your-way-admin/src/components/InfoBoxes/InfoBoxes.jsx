efffdf7d0ee6a845b8bbb0c5cedcfdd65a5f8904
Ali Alrida Shouman <alishouman010@gmail.com>
2022-11-24 10:23:10 +0200

    Frontend: Editing border opacity and color for InfoBoxes component

--- on-your-way-admin/src/components/InfoBoxes/InfoBoxes.jsx
+++ on-your-way-admin/src/components/InfoBoxes/InfoBoxes.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function InfoBoxes({ from, to, pay }) {
   return (
     <div className="flex w-full justify-between ">
-      <div className="border border-x-zinc-50 p-1 rounded-lg w-24   ">
-        <p className="text-xs font-extralight text-zinc-50">From</p>
+      <div className="border border-white border-opacity-40 p-1 rounded-lg w-24   ">
+        <p className="text-xs   text-zinc-50 opacity-70">From</p>
         <p className="font-semibold">{from}</p>
       </div>
-      <div className="border border-x-zinc-50 p-1 rounded-lg w-24 ml-2">
-        <p className="text-xs font-extralight text-zinc-50">To</p>
+      <div className="border border-white border-opacity-40 p-1 rounded-lg w-24 ml-2">
+        <p className="text-xs   text-zinc-50 opacity-70">To</p>
         <p className="font-semibold">{to}</p>
       </div>
-      <div className="border border-x-zinc-50 p-1 rounded-lg w-24 ml-2 ">
-        <p className="text-xs font-extralight text-zinc-50">Pay</p>
+      <div className="border border-white border-opacity-40 p-1 rounded-lg w-24 ml-2 ">
+        <p className="text-xs   text-zinc-50 opacity-70">Pay</p>
         <p className="font-semibold">{pay}</p>
       </div>
     </div>
